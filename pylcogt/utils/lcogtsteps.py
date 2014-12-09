792b12bd8b0023612e52a026faeeb9f6ebc52651
Stefano Valenti <svalenti@lcogt.net>
2014-12-09 10:34:14 -0800

    add run flat

--- pylcogt/utils/lcogtsteps.py
+++ pylcogt/utils/lcogtsteps.py
@@ -21,6 +21,11 @@ def ingest(list_image, table, _force):
         if not exist or _force in ['update', 'yes']:
             hdr = pyfits.getheader(img)
             _instrument = readkey(hdr, 'instrume')
+            if table in ['lcogtraw']:
+                dire = pylcogt.utils.pymysql.rawdata + readkey(hdr,'SITEID') + '/' + readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
+            elif table in ['lcogtredu']:
+                dire = pylcogt.utils.pymysql.workingdirectory + readkey(hdr,'SITEID') + '/' + readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/'
+
             if _instrument in pylcogt.utils.pymysql.instrument0['sbig'] + pylcogt.utils.pymysql.instrument0['sinistro']:
                 print '1m telescope'
                 dictionary = {'dayobs': readkey(hdr, 'DAY-OBS'), 'exptime': readkey(hdr, 'exptime'),
@@ -33,8 +38,8 @@ def ingest(list_image, table, _force):
                     'propid': readkey(hdr, 'PROPID'), 'userid': readkey(hdr, 'USERID'),
                     'dateobs': readkey(hdr, 'DATE-OBS'), 'ccdsum': readkey(hdr, 'CCDSUM')}
                 dictionary['filename'] = string.split(img, '/')[-1]
-                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] + '/' +\
-                                         readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
+                dictionary['filepath'] = dire
+
             elif _instrument in pylcogt.utils.pymysql.instrument0['spectral']:
                 print '2m telescope'
                 dictionary = {'dayobs': readkey(hdr, 'DAY-OBS'), 'exptime': readkey(hdr, 'exptime'),
@@ -45,12 +50,10 @@ def ingest(list_image, table, _force):
                     'ra0': readkey(hdr, 'RA'), 'dec0': readkey(hdr, 'DEC'), 'obstype': readkey(hdr, 'OBSTYPE'),
                     'reqnum': readkey(hdr, 'REQNUM'), 'groupid': readkey(hdr, 'GROUPID'),
                     'propid': readkey(hdr, 'PROPID'), 'userid': readkey(hdr, 'USERID'),
-                    'dateobs': readkey(hdr, 'DATE-OBS'),'ccdsum': readkey(hdr, 'CCDSUM')}
+                    'dateobs': readkey(hdr, 'DATE-OBS'), 'ccdsum': readkey(hdr, 'CCDSUM')}
                 dictionary['namefile'] = string.split(img, '/')[-1]
-                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] +\
-                    readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
-#                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + '1mtel/' + \
-#                                    readkey(hdr, 'DAY-OBS') + '/'
+                dictionary['filepath'] = dire
+
             else:
                 dictionary = ''
         else:
@@ -66,20 +69,18 @@ def ingest(list_image, table, _force):
                 print 'update values'
                 for voce in dictionary:
                     print voce
-
-                    #for voce in ['filepath','ra0','dec0','mjd','exptime','filter','ccdsum']:
-                    for voce in ['ccdsum','filepath']:
-                            pylcogt.utils.pymysql.updatevalue(conn,table,voce,dictionary[voce],
-                                                              string.split(img,'/')[-1],'filename')
+                    # for voce in ['filepath','ra0','dec0','mjd','exptime','filter','ccdsum']:
+                    for voce in ['ccdsum', 'filepath']:
+                            pylcogt.utils.pymysql.updatevalue(conn, table, voce, dictionary[voce],
+                                                              string.split(img, '/')[-1], 'filename')
         else:
             print 'dictionary empty'
 
 #################################################################################################################
 
 
-def run_ingest(telescope,listepoch,_force):
 
-
+def run_ingest(telescope,listepoch,_force,table='lcogtraw'):
     pylcogt.utils.pymysql.site0
     if telescope == 'all':
         tellist = pylcogt.utils.pymysql.site0
@@ -107,7 +108,7 @@ def run_ingest(telescope,listepoch,_force):
                 print imglist
                 if len(imglist):
                     print 'ingest'
-                    ingest(imglist,'lcogtraw',_force)
+                    ingest(imglist,table,_force)
 
 ######################################################################################################################
 
@@ -121,39 +122,74 @@ def tofits(filename, data, hdr=None, clobber=False):
     hdulist.writeto(filename, clobber=clobber, output_verify='ignore')
 
 
-def run_makebias(imagenames, outfilename, minimages=5):
-    biasims = []
+def run_makebias(imagenames, outfilename, minimages=5, clobber=True):
     # Assume the files are all the same number of pixels, should add error checking
     nx = pyfits.getval(imagenames[0], ('NAXIS1'))
     ny = pyfits.getval(imagenames[0], ('NAXIS2'))
     biasdata = np.zeros((len(imagenames), ny, nx))
     for i, f in enumerate(imagenames):
         biasdata[i, :, :] = pyfits.getdata(f)[:, :]
-    if len(biasims) >= minimages:
+    if len(imagenames) >= minimages:
         medbias = np.median(biasdata, axis=0)
-        tofits(outfilename, medbias, clobber=True)
+        tofits(outfilename, medbias, hdr=pyfits.getheader(imagenames[0]), clobber=clobber)
 
 #####################################################################################################################
 
 
-def run_subtractbias(imagenames, outfilenames, masterbiasname, clobber=False):
-    ims = []
-    for f in imagenames:
-        ims.append(ccdproc.CCDData.read(f, unit=u.adu))
-    masterbias = ccdproc.CCDData.read(masterbiasname, unit=u.adu)
-    for i, im in enumerate(ims):
-        d = ccdproc.subtract_bias(im, masterbias)
-        ccdproc.CCDData.write(d, outfilenames[i], clobber=clobber)
+def flatfieldmode(imagearr):
+    # Note that currently we are just bias subtracting and since we are
+    # median combining the bias frames, pixels can only have integer values or 0.5
+    hist = np.histogram(imagearr,
+                        np.round(np.max(imagearr) - np.min(imagearr)) * 2 + 1,
+                        (np.min(imagearr) - 0.25, np.max(imagearr) + 0.25))
+    m = np.argmax(hist[0])
+    return hist[1][m] + 0.25
 
 
-def run_makeflat(imagenames, outfilename, minimages=5):
+def run_subtractbias(imagenames, outfilenames, masterbiasname, clobber=True):
+    # Assume the files are all the same number of pixels, should add error checking
+    biashdu = pyfits.open(masterbiasname)
+    biasdata = biashdu[0].data.copy()
+    biashdu.close()
+
+    for i, im in enumerate(imagenames):
+        hdu = pyfits.open(im)
+        imdata = hdu[0].data.copy()
+        imhdr = hdu[0].header.copy()
+        hdu.close()
+        imdata -= biasdata
+        tofits(outfilenames[i], imdata, hdr=imhdr, clobber=clobber)
+
+
+def run_makeflat(imagenames, outfilename, minimages=3, clobber=True):
     # Flats should already be bias subtracted
-    flatims = []
-    for f in imagenames:
-        flatims.append(ccdproc.CCDData.read(f, unit=u.adu))
-    if len(flatims) >= minimages:
-        flatcombiner = ccdproc.Combiner(flatims)
-        d = flatcombiner.median_combine()
-        ccdproc.CCDData.write(d, outfilename)
 
+    # Load all of the images in normalizing by the mode of the pixel distribution
+    # Assume all of the images have the same size,
+    # FIXME Add error checking
+    nx = pyfits.getval(imagenames[0], ('NAXIS1'))
+    ny = pyfits.getval(imagenames[0], ('NAXIS2'))
+
+    flatdata = np.zeros((len(imagenames), ny, nx))
+    for i, im in enumerate(imagenames):
+        flatdata[i, :, :] = pyfits.getdata(im)[:, :]
+        flatdata[i, :, :] /= flatfieldmode(flatdata[i, :, :])
+    if len(imagenames) >= minimages:
+        medflat = np.median(flatdata, axis=0)
+        tofits(outfilename, medflat, hdr=pyfits.getheader(imagenames[0]),
+               clobber=clobber)
+
+
+def run_flatten(imagenames, outfilenames, masterflatname, clobber=True):
+    flathdu = pyfits.open(masterflatname)
+    flatdata = flathdu[0].data.copy()
+    flathdu.close()
+
+    for i, im in enumerate(imagenames):
+        hdu = pyfits.open(im)
+        imdata = hdu[0].data.copy()
+        imhdr = hdu[0].header.copy()
+        hdu.close()
+        imdata /= flatdata
+        tofits(outfilenames[i], imdata, hdr=imhdr, clobber=clobber)
 
