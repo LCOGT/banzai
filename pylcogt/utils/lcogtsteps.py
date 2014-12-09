8334034f1c492073a4982196e9672d5d82b3155c
cmccully <cmccully@physics.rutgers.edu>
2014-12-08 17:43:05 -0800

    Added flat fielding steps.

--- pylcogt/utils/lcogtsteps.py
+++ pylcogt/utils/lcogtsteps.py
@@ -33,8 +33,8 @@ def ingest(list_image, table, _force):
                     'propid': readkey(hdr, 'PROPID'), 'userid': readkey(hdr, 'USERID'),
                     'dateobs': readkey(hdr, 'DATE-OBS'), 'ccdsum': readkey(hdr, 'CCDSUM')}
                 dictionary['filename'] = string.split(img, '/')[-1]
-                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] + '/' +\
-                                         readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
+                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] + '/' + \
+                                         readkey(hdr, 'instrume') + '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
             elif _instrument in pylcogt.utils.pymysql.instrument0['spectral']:
                 print '2m telescope'
                 dictionary = {'dayobs': readkey(hdr, 'DAY-OBS'), 'exptime': readkey(hdr, 'exptime'),
@@ -45,10 +45,10 @@ def ingest(list_image, table, _force):
                     'ra0': readkey(hdr, 'RA'), 'dec0': readkey(hdr, 'DEC'), 'obstype': readkey(hdr, 'OBSTYPE'),
                     'reqnum': readkey(hdr, 'REQNUM'), 'groupid': readkey(hdr, 'GROUPID'),
                     'propid': readkey(hdr, 'PROPID'), 'userid': readkey(hdr, 'USERID'),
-                    'dateobs': readkey(hdr, 'DATE-OBS'),'ccdsum': readkey(hdr, 'CCDSUM')}
+                    'dateobs': readkey(hdr, 'DATE-OBS'), 'ccdsum': readkey(hdr, 'CCDSUM')}
                 dictionary['namefile'] = string.split(img, '/')[-1]
-                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] +\
-                    readkey(hdr, 'instrume')+ '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
+                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + string.split(img, '/')[-1][0:3] + \
+                    readkey(hdr, 'instrume') + '/' + readkey(hdr, 'DAY-OBS') + '/raw/'
 #                dictionary['filepath'] = pylcogt.utils.pymysql.rawdata + '1mtel/' + \
 #                                    readkey(hdr, 'DAY-OBS') + '/'
             else:
@@ -67,17 +67,17 @@ def ingest(list_image, table, _force):
                 for voce in dictionary:
                     print voce
 
-                    #for voce in ['filepath','ra0','dec0','mjd','exptime','filter','ccdsum']:
-                    for voce in ['ccdsum','filepath']:
-                            pylcogt.utils.pymysql.updatevalue(conn,table,voce,dictionary[voce],
-                                                              string.split(img,'/')[-1],'filename')
+                    # for voce in ['filepath','ra0','dec0','mjd','exptime','filter','ccdsum']:
+                    for voce in ['ccdsum', 'filepath']:
+                            pylcogt.utils.pymysql.updatevalue(conn, table, voce, dictionary[voce],
+                                                              string.split(img, '/')[-1], 'filename')
         else:
             print 'dictionary empty'
 
 #################################################################################################################
 
 
-def run_ingest(telescope,listepoch,_force):
+def run_ingest(telescope, listepoch, _force):
 
 
     pylcogt.utils.pymysql.site0
@@ -107,7 +107,7 @@ def run_ingest(telescope,listepoch,_force):
                 print imglist
                 if len(imglist):
                     print 'ingest'
-                    ingest(imglist,'lcogtraw',_force)
+                    ingest(imglist, 'lcogtraw', _force)
 
 ######################################################################################################################
 
@@ -135,35 +135,60 @@ def run_makebias(imagenames, outfilename, minimages=5, clobber=True):
 #####################################################################################################################
 
 
-def mode(imagearr, precision = 0.01):
-    #make a histogram of the pixel values with 30 bins (which is arbitrary)
-    hist = np.histogram(imagearr, 200)
-    #Find the maximum
-    #Enter a while loop
-    #while the bin width/2 > desired precision
-    #calculate a new histogram covering the maximum bin +-1 bin
-    #Find the new maximum
-    #end while
-    #return the mode (the peak of the pixel distribution)
-
-def run_subtractbias(imagenames, outfilenames, masterbiasname, clobber=False):
-    ims = []
-    for f in imagenames:
-        ims.append(ccdproc.CCDData.read(f, unit=u.adu))
-    masterbias = ccdproc.CCDData.read(masterbiasname, unit=u.adu)
-    for i, im in enumerate(ims):
-        d = ccdproc.subtract_bias(im, masterbias)
-        ccdproc.CCDData.write(d, outfilenames[i], clobber=clobber)
+def flatfieldmode(imagearr):
+    # Note that currently we are just bias subtracting and since we are
+    # median combining the bias frames, pixels can only have integer values or 0.5
+    hist = np.histogram(imagearr,
+                        np.round(np.max(imagearr) - np.min(imagearr)) * 2 + 1,
+                        (np.min(imagearr) - 0.25, np.max(imagearr) + 0.25))
+    m = np.argmax(hist[0])
+    return hist[1][m] + 0.25
 
 
-def run_makeflat(imagenames, outfilename, minimages=5):
+def run_subtractbias(imagenames, outfilenames, masterbiasname, clobber=True):
+    # Assume the files are all the same number of pixels, should add error checking
+    biashdu = pyfits.open(masterbiasname)
+    biasdata = biashdu[0].data.copy()
+    biashdu.close()
+
+    for i, im in enumerate(imagenames):
+        hdu = pyfits.open(im)
+        imdata = hdu[0].data.copy()
+        imhdr = hdu[0].hdr.copy()
+        hdu.close()
+        imdata -= biasdata
+        tofits(outfilenames[i], imdata, hdr=imhdr, clobber=clobber)
+
+
+def run_makeflat(imagenames, outfilename, minimages=3, clobber=True):
     # Flats should already be bias subtracted
-    flatims = []
-    for f in imagenames:
-        flatims.append(ccdproc.CCDData.read(f, unit=u.adu))
-    if len(flatims) >= minimages:
-        flatcombiner = ccdproc.Combiner(flatims)
-        d = flatcombiner.median_combine()
-        ccdproc.CCDData.write(d, outfilename)
 
+    # Load all of the images in normalizing by the mode of the pixel distribution
+    # Assume all of the images have the same size,
+    # FIXME Add error checking
+    nx = pyfits.getval(imagenames[0], ('NAXIS1'))
+    ny = pyfits.getval(imagenames[0], ('NAXIS2'))
+
+    flatdata = np.zeros((len(imagenames), ny, nx))
+    for i, im in enumerate(imagenames):
+        flatdata[i, :, :] = pyfits.getdata(im)[:, :]
+        flatdata[i, :, :] /= flatfieldmode(flatdata[i, :, :])
+    if len(imagenames) >= minimages:
+        medflat = np.median(flatdata, axis=0)
+        tofits(outfilename, medflat, hdr=pyfits.getheader(imagenames[0]),
+               clobber=clobber)
+
+
+def run_flatten(imagenames, outfilenames, masterflatname, clobber=True):
+    flathdu = pyfits.open(masterflatname)
+    flatdata = flathdu[0].data.copy()
+    flathdu.close()
+
+    for i, im in enumerate(imagenames):
+        hdu = pyfits.open(im)
+        imdata = hdu[0].data.copy()
+        imhdr = hdu[0].hdr.copy()
+        hdu.close()
+        imdata /= flatdata
+        tofits(outfilenames[i], imdata, hdr=imhdr, clobber=clobber)
 
