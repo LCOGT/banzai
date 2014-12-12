b1a53c47934a8b8dab05ed7ec15aa7b11d00b32e
Stefano <svalenti@lcogt.net>
2014-12-12 03:44:22 +0000

    add make dark

--- pylcogt/pylcogtmain.py
+++ pylcogt/pylcogtmain.py
@@ -21,7 +21,8 @@ if __name__ == "__main__":
                       help='-T telescope ' + ', '.join(pylcogt.utils.pymysql.telescope0['all']) + ', '.join(
                           pylcogt.utils.pymysql.site0) + ', fts, ftn, 1m0, kb, fl \t [%default]')
     parser.add_option("-s", "--stage", dest="stage", default='', type="str",
-                       help='-s stage [ingest,makebias, makeflat, applybias, applyflat, cosmic, wcs] \t [%default]')
+                       help='-s stage [ingest,makebias, makeflat, makedark, applybias, applydark, '
+                            'applyflat, cosmic, wcs] \t [%default]')
 
     option, args = parser.parse_args()
     # _instrument=option.instrument
@@ -34,7 +35,8 @@ if __name__ == "__main__":
             ['all', 'ftn', 'fts', '1m0', 'kb', 'fl']:
             sys.argv.append('--help')
 
-    if _stage not in ['makebias', 'makeflat', 'applybias', 'applyflat', 'wcs', 'cosmic','ingest','']:
+    if _stage not in ['makebias', 'makedark', 'makeflat', 'applybias', 'applydark', 'applyflat', 'wcs',
+                      'cosmic','ingest','']:
             sys.argv.append('--help')
 
     option, args = parser.parse_args()
@@ -57,7 +59,7 @@ if __name__ == "__main__":
     else:
         if _stage in ['makebias', 'applybias']:
             _table = 'lcogtraw'
-        elif _stage in ['makeflat', 'applyflat', 'cosmic']:
+        elif _stage in ['makedark','makeflat', 'applydark', 'applyflat', 'cosmic']:
             _table = 'lcogtredu'
         else:
             _table = 'lcogtredu'
@@ -128,9 +130,31 @@ if __name__ == "__main__":
                             pylcogt.utils.lcogtsteps.ingest([_output], 'lcogtredu', 'yes')
                             os.system('mv '+_output+' '+directory)
 
+        elif _stage == 'makedark':
+            print 'select dark and make dark'
+            ww = np.asarray([i for i in range(len(ll0['filename'])) if (ll0['obstype'][i] in ['DARK'])])
+            listfile = [k + v for k, v in zip(ll0['filepath'][ww], ll0['filename'][ww])]
+            listbin=ll0['ccdsum'][ww]
+            listinst=ll0['instrument'][ww]
+            listday=ll0['dayobs'][ww]
+            for k in set(listinst):
+                for i in set(listbin):
+                    for j in set(listday):
+                            print k,j,i
+                            listdark=np.array(listfile)[(listbin == i) & (listday == j)]
+                            _output='dark_'+str(k)+'_'+re.sub('-','',str(j))+'_bin'+re.sub(' ','x',i)+'.fits'
+                            pylcogt.utils.lcogtsteps.run_makedark(listdark, _output, minimages=5)
+                            print _output
+                            siteid=pyfits.getheader(_output)['SITEID']
+                            directory=pylcogt.utils.pymysql.workingdirectory+siteid+'/'+k+'/'+re.sub('-','',str(j))+'/'
+                            print 'mv '+_output+' '+directory
+                            pylcogt.utils.lcogtsteps.ingest([_output], 'lcogtredu', 'yes')
+                            os.system('mv '+_output+' '+directory)
+
         elif _stage == 'applybias':
             print 'apply bias to science frame'
-            ww = np.asarray([i for i in range(len(ll0['filename'])) if (ll0['obstype'][i] in ['EXPOSE','SKYFLAT'])])
+            ww = np.asarray([i for i in range(len(ll0['filename'])) if (ll0['obstype'][i] in ['EXPOSE',
+                                                                                              'DARK','SKYFLAT'])])
             listfile = [k + v for k, v in zip(ll0['filepath'][ww], ll0['filename'][ww])]
             listbin=ll0['ccdsum'][ww]
             listinst=ll0['instrument'][ww]
@@ -161,6 +185,38 @@ if __name__ == "__main__":
                                     pylcogt.utils.lcogtsteps.ingest([img], 'lcogtredu', 'no')
                                     os.system('mv '+img+' '+directory)
 
+        elif _stage == 'applydark':
+            print 'apply dark to science frame and flat'
+            ww = np.asarray([i for i in range(len(ll0['filename'])) if (ll0['obstype'][i] in ['EXPOSE', 'SKYFLAT'])])
+            listfile = [k + v for k, v in zip(ll0['filepath'][ww], ll0['filename'][ww])]
+            listbin=ll0['ccdsum'][ww]
+            listinst=ll0['instrument'][ww]
+            listday=ll0['dayobs'][ww]
+            listmjd=ll0['mjd'][ww]
+            listfilt=ll0['filter'][ww]
+            for k in set(listinst):
+                for i in set(listbin):
+                    for j in set(listday):
+                        print j,k,i
+                        listimg = np.array(listfile)[(listbin == i) & (listday == j)]
+                        outfilenames = [re.sub('00.fits','90.fits',string.split(ii,'/')[-1]) for ii in listimg]
+                        listmjd0=np.array(listmjd)[(listbin == i) & (listday == j)]
+                        if len(listimg):
+                            command=['select filepath,filename, mjd-'+str(listmjd0[0])+
+                                     ' as diff from lcogtredu where ccdsum="'+str(i)+'" and instrument = "'+\
+                                     str(k) +'" and obstype = "DARK" and filename like "%dark%" order by diff']
+                            darkgood=pylcogt.utils.pymysql.query(command, conn)
+                            if len(darkgood)>=1:
+                                masterdarkname=darkgood[0]['filepath']+darkgood[0]['filename']
+                                pylcogt.utils.lcogtsteps.run_applydark(listimg, outfilenames, masterdarkname, True)
+                                for img in outfilenames:
+                                    print img
+                                    pylcogt.utils.pymysql.updateheader(img,0, {'DARKCOR':[string.split(masterdarkname,'/')[-1],' dark frame']})
+                                    siteid=pyfits.getheader(img)['SITEID']
+                                    directory=pylcogt.utils.pymysql.workingdirectory+siteid+'/'+k+'/'+re.sub('-','',str(j))+'/'
+                                    print 'mv '+img+' '+directory
+                                    pylcogt.utils.lcogtsteps.ingest([img], 'lcogtredu', 'no')
+                                    os.system('mv '+img+' '+directory)
 
         elif _stage == 'applyflat':
             print 'apply flat to science frame'
@@ -181,7 +237,7 @@ if __name__ == "__main__":
                             if len(listimg)>0:
                                 command=['select filepath,filename, mjd-'+str(listmjd0[0])+' as diff from lcogtredu where ccdsum="'+\
                                          str(i)+'" and instrument = "'+\
-                                         str(k)+'" and filter = "'+str(filt)+'" and obstype="SKYFLAT" order by diff']
+                                         str(k)+'" and filter = "'+str(filt)+'" and obstype="SKYFLAT" and filename like "%flat%" order by diff']
                                 flatgood=pylcogt.utils.pymysql.query(command, conn)
                                 if len(flatgood) >= 1:
                                     masterflatname = flatgood[0]['filepath']+flatgood[0]['filename']
