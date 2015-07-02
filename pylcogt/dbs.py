250f3f78fb995198158597d3ee62d62c6b829869
cmccully <cmccully@lcogt.net>
2015-07-01 17:04:21 -0700

    Added siteid to the database.

--- pylcogt/dbs.py
+++ pylcogt/dbs.py
@@ -27,7 +27,7 @@ class Image(Base):
 
     # Define the table structure
     id = Column(Integer, primary_key=True, autoincrement=True)
-    filename = Column(String(36), index=True)
+    filename = Column(String(36), index=True, unique=True)
     filepath = Column(String(100))
     rawfilename = Column(String(36))
     rawpath = Column(String(100))
@@ -53,12 +53,18 @@ class Image(Base):
     tracknum = Column(String(20))
     reqnum = Column(String(20))
     ccdsum = Column(String(10))
+    siteid = Column(String(10))
+
 
 class Calibration_Image(Base):
     __tablename__ = 'calimages'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
 
 class Reduction_Status(Base):
     __tablename__ = 'reductionstatus'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
 
 def create_db():
     # Create an engine for the database
