c895e72c2463af361ae7ec216ac2d144815b58a2
--global <--global>
2015-07-02 18:11:28 -0700

    Moved utility functions to dateutils.

---
+++ pylcogt/dateutils.py
@@ -0,0 +1,22 @@
+__author__ = 'cmccully'
+
+import datetime
+
+def epoch_string_to_date(epoch):
+    return datetime.date(int(epoch[0:4]), int(epoch[4:6]), int(epoch[6:8]))
+
+def parse_epoch_string(epoch_string):
+    if '-' in epoch_string:
+        epoch1, epoch2 = epoch_string.split('-')
+        start = epoch_string_to_date(epoch1)
+        stop = epoch_string_to_date(epoch2)
+
+        epoch_list = []
+        for i in range((stop - start).days + 1):
+            epoch = start + datetime.timedelta(days=i)
+            epoch_list.append(str(epoch).replace('-',''))
+
+    else:
+        epoch_list = [epoch_string_to_date(epoch_string)]
+
+    return epoch_list
