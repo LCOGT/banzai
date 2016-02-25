2fff6dd96f19643a57d33dca4e5c921be9c763ac
Curtis McCully <cmccully@lcogt.net>
2016-02-24 17:02:05 -0800

    Updates to make bias_maker tests more sensible

--- pylcogt/tests/test_bias_maker.py
+++ pylcogt/tests/test_bias_maker.py
@@ -17,7 +17,7 @@ class FakeImage(Image):
         self.ccdsum = ccdsum
         self.epoch = epoch
         self.instrument = 'get_calibration_filename'
-        self.header = {'OBSTYPE': 'bias'}
+        self.header = {'OBSTYPE': 'BIAS', 'CCDSUM': '2 2'}
         self.filter = 'U'
         self.telescope_id = -1
         self.dateobs = datetime(2016, 1, 1)
@@ -43,9 +43,52 @@ def test_group_by_keywords():
     assert maker.group_by_keywords == ['ccdsum']
 
 
-@mock.patch('pylcogt.bias.fits')
-@mock.patch('pylcogt.bias.dbs')
-def test_raises_an_exection_if_ccdsums_are_different(mock_dbs, mock_fits):
+@mock.patch('pylcogt.bias.Image')
+def test_header_master_bias_level_returns_1(mock_image):
+    maker = BiasMaker(FakeContext())
+
+    maker.do_stage([FakeImage() for x in range(6)])
+
+    args, kwargs = mock_image.call_args
+    header = kwargs['header']
+    assert header['BIASLVL'] == 1.0
+
+
+@mock.patch('pylcogt.bias.Image')
+def test_header_master_bias_level_returns_2(mock_image):
+    maker = BiasMaker(FakeContext())
+
+    maker.do_stage([FakeImage(image_multiplier=2.0) for x in range(6)])
+
+    args, kwargs = mock_image.call_args
+    header = kwargs['header']
+    assert header['BIASLVL'] == 2.0
+
+
+@mock.patch('pylcogt.bias.Image')
+def test_header_cal_type_bias(mock_image):
+
+    maker = BiasMaker(FakeContext())
+
+    maker.do_stage([FakeImage() for x in range(6)])
+
+    args, kwargs = mock_image.call_args
+    header = kwargs['header']
+    assert header['OBSTYPE'].upper() == 'BIAS'
+
+
+@mock.patch('pylcogt.bias.Image')
+def test_ccdsum_header(mock_image):
+    maker = BiasMaker(FakeContext())
+
+    maker.do_stage([FakeImage() for x in range(6)])
+
+    args, kwargs = mock_image.call_args
+    header = kwargs['header']
+    assert header['CCDSUM'] == '2 2'
+
+
+def test_raises_an_exection_if_ccdsums_are_different():
     maker = BiasMaker(FakeContext())
 
     with pytest.raises(InhomogeneousSetException) as exception_info:
@@ -53,9 +96,7 @@ def test_raises_an_exection_if_ccdsums_are_different(mock_dbs, mock_fits):
     assert 'Images have different ccdsums' == str(exception_info.value)
 
 
-@mock.patch('pylcogt.bias.fits')
-@mock.patch('pylcogt.bias.dbs')
-def test_raises_an_exection_if_epochs_are_different(mock_dbs, mock_fits):
+def test_raises_an_exection_if_epochs_are_different():
     maker = BiasMaker(FakeContext())
 
     with pytest.raises(InhomogeneousSetException) as exception_info:
@@ -63,9 +104,7 @@ def test_raises_an_exection_if_epochs_are_different(mock_dbs, mock_fits):
     assert 'Images have different epochs' == str(exception_info.value)
 
 
-@mock.patch('pylcogt.bias.fits')
-@mock.patch('pylcogt.bias.dbs')
-def test_raises_an_exection_if_nx_are_different(mock_dbs, mock_fits):
+def test_raises_an_exection_if_nx_are_different():
     maker = BiasMaker(FakeContext())
 
     with pytest.raises(InhomogeneousSetException) as exception_info:
@@ -73,9 +112,7 @@ def test_raises_an_exection_if_nx_are_different(mock_dbs, mock_fits):
     assert 'Images have different nxs' == str(exception_info.value)
 
 
-@mock.patch('pylcogt.bias.fits')
-@mock.patch('pylcogt.bias.dbs')
-def test_raises_an_exection_if_ny_are_different(mock_dbs, mock_fits):
+def test_raises_an_exection_if_ny_are_different():
     maker = BiasMaker(FakeContext())
 
     with pytest.raises(InhomogeneousSetException) as exception_info:
@@ -83,25 +120,24 @@ def test_raises_an_exection_if_ny_are_different(mock_dbs, mock_fits):
     assert 'Images have different nys' == str(exception_info.value)
 
 
-@mock.patch('pylcogt.bias.dbs')
-@mock.patch('pylcogt.bias.file_utils')
 @mock.patch('pylcogt.bias.Image')
-def test_makes_a_sensible_master_bias(mock_images, mock_fileutils, mock_dbs):
+def test_makes_a_sensible_master_bias(mock_images):
     nimages = 20
     expected_bias = 1183.0
     expected_readnoise = 15.0
 
     images = [FakeImage() for x in range(20)]
     for image in images:
-        image.data = np.random.normal(loc=expected_bias, scale=expected_readnoise, size=(image.ny, image.nx))
+        image.data = np.random.normal(loc=expected_bias, scale=expected_readnoise,
+                                      size=(image.ny, image.nx))
 
     maker = BiasMaker(FakeContext())
     maker.do_stage(images)
 
     args, kwargs = mock_images.call_args
     master_bias = kwargs['data']
-    np.testing.assert_allclose(np.mean(master_bias), 0.0, atol=0.05)
+    np.testing.assert_allclose(np.mean(master_bias), 0.0, atol=0.1)
     actual_bias = float(kwargs['header']['BIASLVL'])
-    assert np.abs(actual_bias - expected_bias) < 0.05
+    assert np.abs(actual_bias - expected_bias) < 0.1
     actual_readnoise = np.std(master_bias)
     assert np.abs(actual_readnoise - expected_readnoise / (nimages ** 0.5)) < 0.2
