ba3c8f5a0003409534e602b1f271107f60e68878
Curtis McCully <cmccully@lcogt.net>
2016-03-15 16:26:32 -0700

    Bugfix in Openmp statement.

--- pylcogt/utils/median_cutils.c
+++ pylcogt/utils/median_cutils.c
@@ -21,7 +21,7 @@ void _cmedian2d(float* d, uint8_t* mask, float* output, int nx, int ny){
     int n_unmasked_pixels;
     int i, j, nxj;
 
-#pragma omp parallel firstprivate(data, output, mask, nx, ny) private(i, j, nxj, n_unmasked_pixels, median_array)
+#pragma omp parallel firstprivate(d, output, mask, nx, ny) private(i, j, nxj, n_unmasked_pixels, median_array)
     {
         /*Each thread allocates its own array. */
         median_array = (float *) malloc(nx * sizeof(float));
