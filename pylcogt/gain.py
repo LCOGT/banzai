603cc85ee27440a5faa78597d23166f74ab1c4d3
Curtis McCully <cmccully@lcogt.net>
2016-03-25 11:52:31 -0700

    Added logging to the gain correction stage.

--- pylcogt/gain.py
+++ pylcogt/gain.py
@@ -1,4 +1,6 @@
 from pylcogt.stages import Stage
+from pylcogt import logs
+import os
 import numpy as np
 
 
@@ -12,6 +14,12 @@ class GainNormalizer(Stage):
 
     def do_stage(self, images):
         for image in images:
+
+            logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+            logs.add_tag(logging_tags, 'filename', os.basename(image.filename))
+            logs.add_tag(logging_tags, 'gain', image.gain)
+            self.logger.debug('Multiplying by gain', extra=logging_tags)
+
             if len(image.data.shape) > 2:
                 n_amps = image.data.shape[0]
                 gain = np.array(eval(image.gain))
