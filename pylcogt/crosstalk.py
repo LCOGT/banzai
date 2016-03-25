ec0428e5bad6211d78038efb160ee22bdbf1c7f3
Curtis McCully <cmccully@lcogt.net>
2016-03-25 12:10:22 -0700

    Added logging to crosstalk correction stage.

--- pylcogt/crosstalk.py
+++ pylcogt/crosstalk.py
@@ -1,5 +1,7 @@
 from pylcogt.stages import Stage
+from pylcogt import logs
 import numpy as np
+import os
 
 
 class CrosstalkCorrector(Stage):
@@ -13,6 +15,8 @@ class CrosstalkCorrector(Stage):
     def do_stage(self, images):
         for image in images:
             if len(image.data.shape) > 2:
+                logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+                logs.add_tag(logging_tags, 'filename', os.path.basename(image.filename))
                 n_amps = image.data.shape[0]
                 crosstalk_matrix = np.identity(n_amps)
                 for j in range(n_amps):
@@ -20,5 +24,8 @@ class CrosstalkCorrector(Stage):
                         if i != j:
                             crosstalk_keyword = 'CRSTLK{0}{1}'.format(i + 1, j + 1)
                             crosstalk_matrix[j, i] = -float(image.header[crosstalk_keyword])
+                            logs.add_tag(logging_tags, crosstalk_keyword,
+                                         image.header[crosstalk_keyword])
+                self.logger.info('Removing crosstalk', extra=logging_tags)
                 image.data = np.dot(crosstalk_matrix, np.swapaxes(image.data, 0, 1))
         return images
