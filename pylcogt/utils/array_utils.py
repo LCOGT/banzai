5f726362cfe6733bdc4cd6c793a2cbf48a3f8d03
Curtis McCully <cmccully@lcogt.net>
2016-03-29 17:11:08 -0700

    Added cast to tuple in array_util slice generator.

--- pylcogt/utils/array_utils.py
+++ pylcogt/utils/array_utils.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import, print_function, division
 
 
 def array_indices_to_slices(a):
-    return (slice(0, x, 1) for x in a.shape)
+    return tuple(slice(0, x, 1) for x in a.shape)
