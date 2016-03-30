61a027a63d4ba5845242065c0c739acea064b64e
Curtis McCully <cmccully@lcogt.net>
2016-03-30 09:53:16 -0700

    Typo fix in fits utils.

--- pylcogt/utils/fits_utils.py
+++ pylcogt/utils/fits_utils.py
@@ -118,29 +118,29 @@ def open_image(filename):
     return data, header, bpm
 
 
-def fits_formats(format_code):		
+def fits_formats(dtype):
     """		
     Convert a numpy data type to a fits format code		
-    :param format_code: dtype parameter from numpy array		
+    :param dtype: dtype parameter from numpy array
     :return: string: Fits type code		
     """		
     format_code = ''		
-    if 'bool' in format_code.name:		
+    if 'bool' in dtype.name:
         format_code = 'L'		
-    elif np.issubdtype(format_code, np.int16):		
+    elif np.issubdtype(dtype, np.int16):
         format_code = 'I'		
-    elif np.issubdtype(format_code, np.int32):		
+    elif np.issubdtype(dtype, np.int32):
         format_code = 'J'		
-    elif np.issubdtype(format_code, np.int64):		
+    elif np.issubdtype(dtype, np.int64):
         format_code = 'K'		
-    elif np.issubdtype(format_code, np.float32):		
+    elif np.issubdtype(dtype, np.float32):
         format_code = 'E'		
-    elif np.issubdtype(format_code, np.float64):		
+    elif np.issubdtype(dtype, np.float64):
         format_code = 'D'		
-    elif np.issubdtype(format_code, np.complex32):		
+    elif np.issubdtype(dtype, np.complex32):
         format_code = 'C'		
-    elif np.issubdtype(format_code, np.complex64):		
+    elif np.issubdtype(dtype, np.complex64):
         format_code = 'M'		
-    elif np.issubdtype(format_code, np.character):		
+    elif np.issubdtype(dtype, np.character):
         format_code = 'A'		
     return format_code		
