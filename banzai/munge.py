2b9f057df856545f875f2d3afac5147e16c1c481
Curtis McCully <cmccully@lcogt.net>
2016-06-07 13:47:32 -0700

    Correctly trap errors when the saturate value is zero (or unknown). Added hard coded value for fs02 in the munge step. This should be measured and put in the header at site! Updated formatting (no changes to the values) of crosstalk coefficients.

--- banzai/munge.py
+++ banzai/munge.py
@@ -1,5 +1,6 @@
 from banzai.stages import Stage
 import numpy as np
+improt os
 from banzai import dbs
 from banzai.utils import array_utils
 
@@ -59,6 +60,22 @@ class DataMunger(Stage):
                 image.header['SATURATE'] = (46000.0, '[ADU] Saturation level used')
             elif '0m4' in telescope.camera_type or '0m8' in telescope.camera_type:
                 image.header['SATURATE'] = (56000.0, '[ADU] Saturation level used')
+            elif 'fs02' == telescope.instrument:
+                # These values were given by Joe Tufts on 2016-06-07
+                # These should really be measured empirically.
+                if image.header['CCDSUM'] == '2 2':
+                    image.header['SATURATE'] = (500000.0 / float(image.header['GAIN']),
+                                                '[ADU] Saturation level used')
+                elif image.header['CCDSUM'] == '1 1':
+                    image.header['SATURATE'] = (125000.0 / float(image.header['GAIN']),
+                                                '[ADU] Saturation level used')
+            # Throw an exception if the saturate value is set to zero
+            try:
+                if float(image.header['SATURATE']) == 0.0:
+                    images.remove(image)
+                    raise ValueError('SATURATE keyword cannot be zero: {filename}'.format(filename=os.path.basename(image.filename)))
+            except ValueError:
+                continue
 
             bpm_slices = array_utils.array_indices_to_slices(image.bpm)
             image.bpm[image.data[bpm_slices] >= float(image.header['SATURATE'])] = 2
@@ -80,36 +97,36 @@ def set_crosstalk_header_keywords(image):
                 crosstalk_comment = '[Crosstalk coefficient] Q{i} signal due to Q{j}'.format(i=i+1, j=j+1)
                 image.header['CRSTLK{0}{1}'.format(i + 1, j + 1)] = (coefficients[j, i], crosstalk_comment)
 
-crosstalk_coefficients = {'fl01': np.array([[0.0, 0.00074, 0.00081, 0.00115],
-                                            [0.0007, 0.0, 0.00118, 0.00085],
-                                            [0.00076, 0.00115, 0.0, 0.00088],
-                                            [0.00107, 0.00075, 0.0008, 0.0]]),
-                          'fl02': np.array([[0.0, 0.00084, 0.00088, 0.00125],
-                                            [0.00083, 0.0, 0.00124, 0.00096],
-                                            [0.00086, 0.00121, 0.0, 0.00098],
-                                            [0.00116, 0.00085, 0.00092, 0.0]]),
-                          'fl03': np.array([[0.0, 0.00076, 0.00079, 0.00115],
-                                            [0.00073, 0.0, 0.00117, 0.00084],
-                                            [0.00074, 0.00113, 0.0, 0.00084],
-                                            [0.00105, 0.00075, 0.0008, 0.0]]),
-                          'fl04': np.array([[0.0, 0.00088, 0.00096, 0.00131],
-                                            [0.00087, 0.0, 0.00132, 0.00099],
-                                            [0.00087, 0.00127, 0.0, 0.00103],
-                                            [0.00123, 0.00089, 0.00094, 0.0]]),
-                          'fl05': np.array([[0.0, 0.00084, 0.00090, 0.00126],
-                                            [0.00089, 0.0, 0.00133, 0.00095],
-                                            [0.00097, 0.00155, 0.0, 0.00108],
-                                            [0.00134, 0.00096, 0.00095, 0.0]]),
-                          'fl06': np.array([[0.0, 0.00078, 0.00085, 0.00118],
-                                            [0.00075, 0.0, 0.00112, 0.00087],
-                                            [0.00074, 0.00110, 0.0, 0.00089],
-                                            [0.00098, 0.00075, 0.00081, 0.0]]),
-                          'fl07': np.array([[0.0, 0.00075, 0.00077, 0.00113],
-                                            [0.00071, 0.0, 0.00113, 0.00082],
-                                            [0.0007, 0.00108, 0.0, 0.00086],
-                                            [0.00095, 0.00067, 0.00077, 0.0]]),
-                          'fl08': np.array([[0.0, 0.00057, 0.00078, 0.0013],
-                                            [0.00112, 0.0, 0.00163, 0.00123],
-                                            [0.00104, 0.00113, 0.0, 0.00113],
-                                            [0.00108, 0.00048, 0.00065, 0.0]])
+crosstalk_coefficients = {'fl01': np.array([[0.00000, 0.00074, 0.00081, 0.00115],
+                                            [0.00070, 0.00000, 0.00118, 0.00085],
+                                            [0.00076, 0.00115, 0.00000, 0.00088],
+                                            [0.00107, 0.00075, 0.00080, 0.00000]]),
+                          'fl02': np.array([[0.00000, 0.00084, 0.00088, 0.00125],
+                                            [0.00083, 0.00000, 0.00124, 0.00096],
+                                            [0.00086, 0.00121, 0.00000, 0.00098],
+                                            [0.00116, 0.00085, 0.00092, 0.00000]]),
+                          'fl03': np.array([[0.00000, 0.00076, 0.00079, 0.00115],
+                                            [0.00073, 0.00000, 0.00117, 0.00084],
+                                            [0.00074, 0.00113, 0.00000, 0.00084],
+                                            [0.00105, 0.00075, 0.00080, 0.00000]]),
+                          'fl04': np.array([[0.00000, 0.00088, 0.00096, 0.00131],
+                                            [0.00087, 0.00000, 0.00132, 0.00099],
+                                            [0.00087, 0.00127, 0.00000, 0.00103],
+                                            [0.00123, 0.00089, 0.00094, 0.00000]]),
+                          'fl05': np.array([[0.00000, 0.00084, 0.00090, 0.00126],
+                                            [0.00089, 0.00000, 0.00133, 0.00095],
+                                            [0.00097, 0.00155, 0.00000, 0.00108],
+                                            [0.00134, 0.00096, 0.00095, 0.00000]]),
+                          'fl06': np.array([[0.00000, 0.00078, 0.00085, 0.00118],
+                                            [0.00075, 0.00000, 0.00112, 0.00087],
+                                            [0.00074, 0.00110, 0.00000, 0.00089],
+                                            [0.00098, 0.00075, 0.00081, 0.00000]]),
+                          'fl07': np.array([[0.00000, 0.00075, 0.00077, 0.00113],
+                                            [0.00071, 0.00000, 0.00113, 0.00082],
+                                            [0.00070, 0.00108, 0.00000, 0.00086],
+                                            [0.00095, 0.00067, 0.00077, 0.00000]]),
+                          'fl08': np.array([[0.00000, 0.00057, 0.00078, 0.00130],
+                                            [0.00112, 0.00000, 0.00163, 0.00123],
+                                            [0.00104, 0.00113, 0.00000, 0.00113],
+                                            [0.00108, 0.00048, 0.00065, 0.00000]])
                           }
