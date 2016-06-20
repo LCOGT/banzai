ee42188ee667eddc87edfd7e3b12ced98279c981
Curtis McCully <cmccully@lcogt.net>
2016-06-20 13:24:19 -0700

    Bugfix in trimmer if the TRIMSEC keyword is not set.

--- banzai/trim.py
+++ banzai/trim.py
@@ -8,21 +8,29 @@ import os
 
 __author__ = 'cmccully'
 
+logger = logs.get_logger(__name__)
+
 
 def _trim_image(image):
     trimsec = fits_utils.parse_region_keyword(image.header['TRIMSEC'])
 
-    image.data = image.data[trimsec]
-    image.bpm = image.bpm[trimsec]
+    if trimsec is not None:
+        image.data = image.data[trimsec]
+        image.bpm = image.bpm[trimsec]
 
-    # Update the NAXIS and CRPIX keywords
-    image.header['NAXIS1'] = trimsec[1].stop - trimsec[1].start
-    image.header['NAXIS2'] = trimsec[0].stop - trimsec[0].start
-    image.header['CRPIX1'] -= trimsec[1].start
-    image.header['CRPIX2'] -= trimsec[0].start
+        # Update the NAXIS and CRPIX keywords
+        image.header['NAXIS1'] = trimsec[1].stop - trimsec[1].start
+        image.header['NAXIS2'] = trimsec[0].stop - trimsec[0].start
+        image.header['CRPIX1'] -= trimsec[1].start
+        image.header['CRPIX2'] -= trimsec[0].start
 
-    image.header['L1STATTR'] = (1, 'Status flag for overscan trimming')
-
+        image.header['L1STATTR'] = (1, 'Status flag for overscan trimming')
+    else:
+        logging_tags = logs.image_config_to_tags(image, None)
+        logs.add_tag(logging_tags, 'filename', os.path.basename(image.filename))
+        logs.add_tag(logging_tags, 'trimsec', image.header['TRIMSEC'])
+        logger.warning('TRIMSEC was not defined.', extra=logging_tags)
+        image.header['L1STATTR'] = (0, 'Status flag for overscan trimming')
     return image.header['NAXIS1'], image.header['NAXIS2']
 
 
