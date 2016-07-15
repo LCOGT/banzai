b7ff4dc7f896bbcbe514493b32b00f8c5846dd8a
ebachelet <etibachelet@gmail.com>
2016-07-15 16:33:51 -0700

    Implemetation

---
+++ banzai/qc/header_checker.py
@@ -0,0 +1,110 @@
+from banzai.stages import Stage
+from banzai import logs
+
+import os
+
+
+class HeaderSanity(Stage):
+    """
+    Check the header of the image.
+
+    """
+
+    def __init__(self, pipeline_context):
+        super(HeaderSanity, self).__init__(pipeline_context)
+
+        self.Header_expected_format = {'RA': str, 'DEC': str, 'CAT-RA': str, 'CAT-DEC': str, 'OFST-RA': str,
+                                       'OFST-DEC': str, 'TPT-RA': str, 'TPT-DEC': str, 'PM-RA': str, 'PM-DEC': str,
+                                       'CRVAL1': float, 'CRVAL2': float, 'EXPTIME': float}
+
+    @property
+    def group_by_keywords(self):
+        return None
+
+    def do_stage(self, images):
+
+        for image in images:
+
+            for keyword in self.Header_expected_format.keys():
+                self.check_header_format(keyword, image)
+                self.check_header_NA(keyword, image)
+
+            self.check_RA_range(image)
+            self.check_DEC_range(image)
+            self.check_EXPTIME_value(image)
+
+        return images
+
+    def check_header_format(self, keyword, image):
+
+        header_value = image.header[keyword]
+
+        logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+
+        if not isinstance(header_value, self.Header_expected_format[keyword]):
+            sentence = 'The header key ' + keyword + ' got an unexpected format : ' + type(header_value).__name__ + \
+                       ' in place of ' + self.Header_expected_format[keyword].__name__
+
+            self.logger.error(sentence, extra=logging_tags)
+
+            return
+
+    def check_header_NA(self, keyword, image):
+
+        header_value = image.header[keyword]
+
+        logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+
+        if isinstance(header_value, str):
+
+            if ('N/A' in header_value):
+                sentence = 'The header key ' + keyword + ' got the unexpected value : N/A'
+                self.logger.error(sentence, extra=logging_tags)
+
+        return
+
+    def check_RA_range(self, image):
+
+        RA_value = image.header['CRVAL1']
+
+        logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+
+        if isinstance(RA_value, float):
+
+            if (RA_value > 360) | (RA_value < 0):
+                sentence = 'The header CRVAL1 key got the unexpected value : ' + str(RA_value)
+                self.logger.error(sentence, extra=logging_tags)
+
+        return
+
+    def check_DEC_range(self, image):
+
+        DEC_value = image.header['CRVAL2']
+
+        logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+
+        if (DEC_value > 90) | (DEC_value < -90):
+            sentence = 'The header CRVAL2 key got the unexpected value : ' + str(DEC_value)
+            self.logger.error(sentence, extra=logging_tags)
+
+        return
+
+    def check_EXPTIME_value(self, image):
+
+        EXPTIME_value = image.header['EXPTIME']
+
+
+        logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+
+        if EXPTIME_value < 0.0:
+            sentence = 'The header EXPTIME key got the unexpected value : negative value'
+            self.logger.error(sentence, extra=logging_tags)
+
+            return
+
+        OBSTYPE = image.header['OBSTYPE']
+        if (EXPTIME_value == 0.0) & (OBSTYPE == 'EXPOSE'):
+            sentence = 'The header EXPTIME key got the unexpected value : 0.0'
+            self.logger.error(sentence, extra=logging_tags)
+
+            return
---
+++ banzai/tests/test_header_checker.py
@@ -0,0 +1,113 @@
+from .utils import FakeImage
+from banzai.qc import header_checker
+
+
+import mock
+import numpy as np
+
+def test_no_input_images():
+    tester = header_checker.HeaderSanity(None)
+    images = tester.do_stage([])
+    assert len(images) == 0
+
+
+def test_group_by_keywords():
+    tester = header_checker.HeaderSanity(None)
+    assert tester.group_by_keywords is None
+
+
+def test_bad_RA_format():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(6)]
+    for image in images:
+        image.header['RA'] = 20
+
+        tester.check_header_format('RA', image)
+        assert tester.logger.error.called
+
+    assert len(images) == 6
+
+
+def test_NA_in_RA():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(6)]
+    for image in images:
+        image.header['RA'] = 'N/A'
+
+        tester.check_header_NA('RA', image)
+        assert tester.logger.error.called
+
+    assert len(images) == 6
+
+
+def test_RA_outside_range():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(8)]
+    for image in images:
+        image.header['CRVAL1'] = 7892.28
+
+        tester.check_RA_range(image)
+        assert tester.logger.error.called
+
+    assert len(images) == 8
+
+
+def test_DEC_outside_range():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
+    for image in images:
+        image.header['CRVAL2'] = -189.52
+
+        tester.check_DEC_range(image)
+        assert tester.logger.error.called
+
+    assert len(images) == 5
+
+
+def test_negative_EXPTIME_value():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
+    for image in images:
+        image.header['EXPTIME'] = -np.random.uniform(0, 1000)
+
+        tester.check_EXPTIME_value(image)
+        assert tester.logger.error.called
+
+    assert len(images) == 5
+
+
+def test_nulle_EXPTIME_value_on_science_frames():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    nx = 101
+    ny = 103
+
+    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
+    for image in images:
+        image.header['EXPTIME'] = 0.0
+        image.header['OBSTYPE'] = 'EXPOSE'
+
+        tester.check_EXPTIME_value(image)
+        assert tester.logger.error.called
+
+    assert len(images) == 5
