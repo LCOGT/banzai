9f7e5b0a1eb33defd40453bb8b9430ad2f0ae880
Monica Turner <mturner@lco.global>
2018-04-23 19:59:44 +0000

    Posts all results to elasticsearch

--- banzai/qc/pattern_noise.py
+++ banzai/qc/pattern_noise.py
@@ -1,13 +1,16 @@
-from banzai.stages import Stage
-from banzai.utils.stats import median_absolute_deviation
-from banzai.qc.utils import save_qc_results
+import os
 import numpy as np
 
-class PatternNoiseDetector(Stage):
+from banzai.qc.qc_stage import QCStage
+from banzai import logs
+from banzai.utils.stats import median_absolute_deviation
+
+
+class PatternNoiseDetector(QCStage):
     # Signal to Noise threshold to raise an alert
-    snr_threshold = 15.0
+    SNR_THRESHOLD = 15.0
     # Number of pixels that need to be above the S/N threshold to raise an alert
-    pixel_threshold = 5
+    PIXEL_THRESHOLD = 5
 
     def __init__(self, pipeline_context):
         super(PatternNoiseDetector, self).__init__(pipeline_context)
@@ -17,16 +20,26 @@ class PatternNoiseDetector(Stage):
         return None
 
     def do_stage(self, images):
+        images_to_remove = []
         for image in images:
             # If the data is a cube, then run on each extension individually
+            logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
+            logs.add_tag(logging_tags, 'filename', os.path.basename(image.filename))
+            logs.add_tag(logging_tags, 'snr_threshold', self.SNR_THRESHOLD)
+            logs.add_tag(logging_tags, 'pixel_threshold', self.PIXEL_THRESHOLD)
             if len(image.data.shape) > 2:
-                for data in image.data:
-                    pattern_noise_is_bad = check_for_pattern_noise(data, self.snr_threshold, self.pixel_threshold)
+                pattern_noise_is_bad = any([check_for_pattern_noise(data, self.SNR_THRESHOLD, self.PIXEL_THRESHOLD)
+                                            for data in image.data])
             else:
-                pattern_noise_is_bad = check_for_pattern_noise(image.data, self.snr_threshold, self.pixel_threshold)
-
+                pattern_noise_is_bad = check_for_pattern_noise(image.data, self.SNR_THRESHOLD, self.PIXEL_THRESHOLD)
             if pattern_noise_is_bad:
-                save_qc_results({'PatternNoise': True}, image)
+                self.logger.error('Image found to have pattern noise.', extra=logging_tags)
+                images_to_remove.append(image)
+            else:
+                self.logger.info('No pattern noise found.', extra=logging_tags)
+            self.save_qc_results({'PatternNoise': pattern_noise_is_bad}, image)
+        for image in images_to_remove:
+            images.remove(image)
         return images
 
 
