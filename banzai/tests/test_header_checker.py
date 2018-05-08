331cf3c0525a4ca80519e9a2581f80caae79ee01
turnerm <mturner@lco.global>
2018-05-08 23:06:57 +0000

    Updated tests to new header checker format

--- banzai/tests/test_header_checker.py
+++ banzai/tests/test_header_checker.py
@@ -3,7 +3,16 @@ from banzai.qc import header_checker
 
 
 import mock
-import numpy as np
+
+
+class FakeHeaderImage(FakeImage):
+    def __init__(self, header=None):
+        super(FakeHeaderImage, self).__init__()
+        if header is None:
+            self.header = {}
+        else:
+            self.header = header
+
 
 def test_no_input_images():
     tester = header_checker.HeaderSanity(None)
@@ -15,113 +24,91 @@ def test_group_by_keywords():
     tester = header_checker.HeaderSanity(None)
     assert tester.group_by_keywords is None
 
-def test_no_exptime_in_frames():
+
+def test_all_keywords_missing():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
+    bad_keywords = tester.check_keywords_missing_or_na(FakeHeaderImage())
+    assert set(bad_keywords) == set(tester.header_keywords_list)
 
-    images = [FakeImage(nx=nx, ny=ny) for x in range(6)]
 
-    for image in images:
+def test_all_keywords_na():
+    tester = header_checker.HeaderSanity(None)
+    image = FakeHeaderImage({keyword: "N/A" for keyword in tester.header_keywords_list})
+    bad_keywords = tester.check_keywords_missing_or_na(image)
+    assert set(bad_keywords) == set(tester.header_keywords_list)
 
-        tester.check_header_keyword_present(['EXPTIME'], image)
-        assert tester.logger.error.called
 
-    assert len(images) == 6
+def test_all_keywords_okay():
+    tester = header_checker.HeaderSanity(None)
+    image = FakeHeaderImage({keyword: "test" for keyword in tester.header_keywords_list})
+    bad_keywords = tester.check_keywords_missing_or_na(image)
+    assert set(bad_keywords) == set([])
 
-def test_bad_RA_format():
+
+def test_one_keyword_missing_and_one_na():
+    tester = header_checker.HeaderSanity(None)
+    image = FakeHeaderImage({keyword: "test" for keyword in tester.header_keywords_list[1:]})
+    image.header[tester.header_keywords_list[1]] = 'N/A'
+    bad_keywords = tester.check_keywords_missing_or_na(image)
+    assert set(bad_keywords) == set(tester.header_keywords_list[0:2])
+
+
+def test_ra_outside_range():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
-
-    images = [FakeImage(nx=nx, ny=ny) for x in range(6)]
-    for image in images:
-        image.header['RA'] = 20
-
-        tester.check_header_format('RA', image)
-        assert tester.logger.error.called
-
-    assert len(images) == 6
+    tester.check_ra_range(FakeHeaderImage({'CRVAL1': 360.1}))
+    assert tester.logger.error.called
 
 
-def test_NA_in_RA():
+def test_ra_within_range():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
-
-    images = [FakeImage(nx=nx, ny=ny) for x in range(6)]
-    for image in images:
-        image.header['RA'] = 'N/A'
-
-        tester.check_header_na(['RA'], image)
-        assert tester.logger.error.called
-
-    assert len(images) == 6
+    tester.check_ra_range(FakeHeaderImage({'CRVAL1': 359.9}))
+    tester.check_ra_range(FakeHeaderImage({'CRVAL1': 0.1}))
+    assert not tester.logger.error.called
 
 
-def test_RA_outside_range():
+def test_dec_outside_range():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
-
-    images = [FakeImage(nx=nx, ny=ny) for x in range(8)]
-    for image in images:
-        image.header['CRVAL1'] = 7892.28
-
-        tester.check_ra_range(image)
-        assert tester.logger.error.called
-
-    assert len(images) == 8
+    tester.check_dec_range(FakeHeaderImage({'CRVAL2': -90.1}))
+    assert tester.logger.error.called
 
 
-def test_DEC_outside_range():
+def test_dec_within_range():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
-
-    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
-    for image in images:
-        image.header['CRVAL2'] = -189.52
-
-        tester.check_dec_range(image)
-        assert tester.logger.error.called
-
-    assert len(images) == 5
+    tester.check_dec_range(FakeHeaderImage({'CRVAL2': -89.9}))
+    tester.check_dec_range(FakeHeaderImage({'CRVAL2': 89.9}))
+    tester.check_dec_range(FakeHeaderImage({'CRVAL2': 0.0}))
+    assert not tester.logger.error.called
 
 
-def test_negative_EXPTIME_value():
+def test_null_exptime_value():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
-
-    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
-    for image in images:
-        image.header['EXPTIME'] = -np.random.uniform(0, 1000)
-
-        tester.check_exptime_value(image)
-        assert tester.logger.error.called
-
-    assert len(images) == 5
+    tester.check_exptime_value(FakeHeaderImage({'EXPTIME': 0.0, 'OBSTYPE': 'test'}))
+    assert tester.logger.error.called
 
 
-def test_nulle_EXPTIME_value_on_science_frames():
+def test_negative_exptime_value():
     tester = header_checker.HeaderSanity(None)
     tester.logger.error = mock.MagicMock()
-    nx = 101
-    ny = 103
+    tester.check_exptime_value(FakeHeaderImage({'EXPTIME': -0.1, 'OBSTYPE': 'test'}))
+    assert tester.logger.error.called
 
-    images = [FakeImage(nx=nx, ny=ny) for x in range(5)]
-    for image in images:
-        image.header['EXPTIME'] = 0.0
-        image.header['OBSTYPE'] = 'EXPOSE'
 
-        tester.check_exptime_value(image)
-        assert tester.logger.error.called
+def test_null_or_negative_exptime_value_for_bias():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    tester.check_exptime_value(FakeHeaderImage({'EXPTIME': 0.0, 'OBSTYPE': 'BIAS'}))
+    tester.check_exptime_value(FakeHeaderImage({'EXPTIME': -0.1, 'OBSTYPE': 'BIAS'}))
+    assert not tester.logger.error.called
 
-    assert len(images) == 5
+
+def test_postive_exptime_value():
+    tester = header_checker.HeaderSanity(None)
+    tester.logger.error = mock.MagicMock()
+    tester.check_exptime_value(FakeHeaderImage({'EXPTIME': 0.1, 'OBSTYPE': 'test'}))
+    assert not tester.logger.error.called
