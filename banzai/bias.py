7555d192b436ed0785e59021bf2070665a9f29fa
turnerm <mturner@lco.global>
2018-05-15 20:51:05 +0000

    Remove level subtraction bias subtraction class

--- banzai/bias.py
+++ banzai/bias.py
@@ -96,7 +96,6 @@ class BiasSubtractor(ApplyCalibration):
         for image in images:
             logs.add_tag(logging_tags, 'filename', os.path.basename(image.filename))
 
-            image.subtract(master_bias_level)
             image.subtract(master_bias_data)
 
             image.bpm |= master_calibration_image.bpm
