9505865225053029daa271727b058126a3bf3035
turnerm <mturner@lco.global>
2018-07-27 22:28:38 +0000

    Refactored

--- banzai/qc/pattern_noise.py
+++ banzai/qc/pattern_noise.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from scipy.signal import cwt, ricker
+from scipy.ndimage.filters import median_filter
 
 from banzai.stages import Stage
 from banzai import logs
@@ -8,15 +9,9 @@ from banzai import logs
 
 class PatternNoiseDetector(Stage):
     # Signal to Noise threshold to raise an alert
-    SNR_THRESHOLD = 5.0
-    # Fraction of convolution widths that need to be above S/N threshold to raise an alert
-    FRAC_WIDTHS_THRESHOLD = 0.9
+    SNR_THRESHOLD = 10.0
     # The maximum allowed standard deviation of the peak centres
     STD_THRESHOLD = 1.0
-    # Minimum convolution wavelet minimum
-    WAVELET_WIDTH_MIN = 25
-    # Maximum convolution wavelet minimum
-    WAVELET_WIDTH_MAX = 50
 
     def __init__(self, pipeline_context):
         super(PatternNoiseDetector, self).__init__(pipeline_context)
@@ -30,10 +25,7 @@ class PatternNoiseDetector(Stage):
             logging_tags = logs.image_config_to_tags(image, self.group_by_keywords)
             logs.add_tag(logging_tags, 'filename', os.path.basename(image.filename))
             logs.add_tag(logging_tags, 'snr_threshold', self.SNR_THRESHOLD)
-            logs.add_tag(logging_tags, 'frac_widths_threshold', self.FRAC_WIDTHS_THRESHOLD)
             logs.add_tag(logging_tags, 'std_threshold', self.STD_THRESHOLD)
-            logs.add_tag(logging_tags, 'wavelet_width_min', self.WAVELET_WIDTH_MIN)
-            logs.add_tag(logging_tags, 'wavelet_width_max', self.WAVELET_WIDTH_MAX)
 
             # If the data is a cube, then run on each extension individually
             if image.data_is_3d():
@@ -46,15 +38,12 @@ class PatternNoiseDetector(Stage):
             else:
                 self.logger.info('No pattern noise found.', extra=logging_tags)
             self.save_qc_results({'pattern_noise.failed': pattern_noise_is_bad,
-                                  'pattern_noise.wavelet_width_min': self.WAVELET_WIDTH_MIN,
-                                  'pattern_noise.wavelet_width_max': self.WAVELET_WIDTH_MAX,
                                   'pattern_noise.snr_threshold': self.SNR_THRESHOLD,
-                                  'pattern_noise.frac_widths_threshold': self.FRAC_WIDTHS_THRESHOLD,
                                   'pattern_noise.std_threshold': self.STD_THRESHOLD,
                                   }, image)
         return images
 
-    def check_for_pattern_noise(self, data, image_clip=100, band_width=0.25, snr_clip_fraction=0.05):
+    def check_for_pattern_noise(self, data):
         """
         Test for pattern noise in an image
 
@@ -62,8 +51,6 @@ class PatternNoiseDetector(Stage):
         ----------
         data : numpy array
                Image data to test for pattern noise
-        image_clip : int
-                Number of pixels to clip from each edge
         band_width : float
                 The fractional width of the central band to extract from the FFT
         snr_clip_fraction: float
@@ -76,39 +63,156 @@ class PatternNoiseDetector(Stage):
 
         """
 
-        # Throw away image edges
-        data = data[image_clip:-image_clip, image_clip:-image_clip]
+        trimmed_data = trim_image_edges(data)
+        power_2d = get_2d_power_band(trimmed_data)
+        snr = compute_snr(power_2d)
+        convolved_snr = convolve_snr_with_wavelet(snr)
+        peak_maxima, std_maxima = get_peak_parameters(convolved_snr)
 
-        # Get full 2D power
-        full_power_2d = np.abs(np.fft.rfft2(data))
-
-        # Extract horizontal band, as corners of 2D FFT can vary significantly between images
-        ny = full_power_2d.shape[0]
-        y1 = round(ny * (0.5 - band_width/2))
-        y2 = round(ny * (0.5 + band_width/2))
-        power_2d = full_power_2d[y1:y2]
-
-        # Get 1D SNR
-        power = np.median(power_2d, axis=0)[1:]  # Throw away DC term
-        p2p_scatter = (np.sum((power[1:]-power[:-1])**2) / (len(power)-2))**0.5
-        snr = (power - np.median(power)) / p2p_scatter
-
-        # Convolve with wavelet to find peaks, throwing away edges
-        widths = np.arange(self.WAVELET_WIDTH_MIN, self.WAVELET_WIDTH_MAX)
-        snr_convolved = cwt(snr, ricker, widths)
-        n_snr_clip = round(snr_clip_fraction * len(snr))
-        snr_convolved = snr_convolved[:, n_snr_clip:-n_snr_clip]
-
-        # Calculate fraction of peaks above threshold and the standard deviation of their centres
-        peak_maxima = np.max(snr_convolved, axis=1)
-        frac_maxima_above_snr_thresh = sum(peak_maxima > self.SNR_THRESHOLD) / len(peak_maxima)
-        peak_indices = np.argmax(snr_convolved, axis=1)
-        std_maxima = np.std(peak_indices)
-
-        # Check that enough peaks are above threshold and that the peak center standard deviation is small.
-        # Also, the result cannot be trusted if any of the peaks are at the edge of the SNR array.
-        has_pattern_noise = (frac_maxima_above_snr_thresh > self.FRAC_WIDTHS_THRESHOLD
-                             and std_maxima < self.STD_THRESHOLD
-                             and not any(peak_indices == 0)
-                             and not any(peak_indices == len(snr_convolved)-1))
+        # Check that all peaks are above threshold and that the peak center standard deviation is small.
+        has_pattern_noise = (peak_maxima > self.SNR_THRESHOLD).all() and std_maxima < self.STD_THRESHOLD
         return has_pattern_noise
+
+
+def trim_image_edges(data, fractional_edge_width=0.025):
+    """
+    Clip image edges to avoid edge effects in Fourier transform
+
+    Parameters
+    ----------
+    data : numpy array
+        The data to be trimmed
+    fractional_edge_width : float
+        Fraction of mean(nx, ny) to trim from each edge
+
+    Returns
+    -------
+    trimmed_data : numpy array
+        Trimmed data array
+    """
+    ntrim = int(round(np.mean(data.shape) * fractional_edge_width))
+    return data[ntrim:-ntrim, ntrim:-ntrim]
+
+
+def get_2d_power_band(data, fractional_band_width=0.25):
+    """
+    Extract the central region of the 2D Fourier transform
+
+    Parameters
+    ----------
+    data : numpy array
+        The data for computing the Fourier Transform
+    fractional_band_width : float
+        Vertical band width as a fraction of ny
+
+    Returns
+    -------
+    power_2d : numpy array
+        Central band of 2d Fourier transform
+    """
+    # Get full 2D power
+    full_power_2d = abs(np.fft.rfft2(data))
+
+    # Extract horizontal band, as corners of 2D FFT can vary significantly between images
+    ny = full_power_2d.shape[0]
+    y1 = round(ny * (0.5 - fractional_band_width/2))
+    y2 = round(ny * (0.5 + fractional_band_width/2))
+
+    return full_power_2d[y1:y2]
+
+
+def compute_snr(power_2d, fractional_window_size=0.05):
+    """
+    Extract the central region of the 2D Fourier transform
+
+    Parameters
+    ----------
+    power_2d : numpy array
+        The 2D Fourier transform of the data
+    fractional_window_size : float
+        Median filter window size as a fraction of the 1D power array
+
+    Returns
+    -------
+    snr : numpy array
+        The 1D SNR
+    """
+    power = np.median(power_2d, axis=0)
+    p2p_scatter = abs(power[1:] - power[:-1])
+    power = power[1:]  # Throw away DC term
+
+    # Median filter
+    window_size = get_odd_integer(fractional_window_size * len(power))
+    continuum = median_filter(power, size=window_size)
+    scatter = median_filter(p2p_scatter, size=window_size)
+
+    snr = (power - continuum) / scatter
+    return snr
+
+
+def get_odd_integer(x):
+    """
+    Return the closest odd integer given a float
+
+    Parameters
+    ----------
+    x : float
+        The number to be converted to the closest odd integer
+
+    Returns
+    -------
+    y : int
+        Odd integer of x
+    """
+    return int(round(round(x) / 2) * 2) + 1
+
+
+def convolve_snr_with_wavelet(snr,
+                              fractional_wavelet_width_min=0.01,
+                              fractional_wavelet_width_max=0.025,
+                              nwavelets=25):
+    """
+    Convolve the 1D SNR with nwavelets Ricker wavelets, with widths ranging from
+    fractional_wavelength_width_min * len(snr) to fractional_wavelength_witdh_max * len(snr).
+
+    Parameters
+    ----------
+    snr : numpy array
+        The 1D SNR array
+    fractional_wavelet_width_min : float
+        The fraction of the SNR length to use as the minimum wavelet width
+    fractional_wavelet_width_max : float
+        The fraction of the SNR length to use as the maximum wavelet width
+    nwavelets : int
+        The number of wavelength widths to convolve with the SNR
+
+    Returns
+    -------
+    snr_convolved : numpy array
+        Array of size nwavelets x len(snr) of snr convolved with different wavelet widths
+    """
+
+    widths = np.linspace(len(snr) * fractional_wavelet_width_min,
+                         len(snr) * fractional_wavelet_width_max,
+                         nwavelets)
+    return cwt(snr, ricker, widths)
+
+
+def get_peak_parameters(convolved_snr):
+    """
+
+    Parameters
+    ----------
+    convolved_snr : numpy array
+        The SNR array convolved with the Ricker wavelet of various widths
+
+    Returns
+    -------
+    peak_maxima: array
+        The the maximum peak value for all Ricker wavelet widths
+    std_maxima: float
+        The standard devation of the peak maxima across wavelet widths
+    """
+    peak_maxima = np.max(convolved_snr, axis=1)
+    std_maxima = np.std(np.argmax(convolved_snr, axis=1))
+    return peak_maxima, std_maxima
