f5818de94d306d02dccc20958ba5045daf83213c
turnerm <mturner@lco.global>
2018-08-23 15:50:25 +0000

    Removed comma

--- banzai/utils/qc.py
+++ banzai/utils/qc.py
@@ -10,7 +10,7 @@ def format_qc_results(qc_results, image):
                        'molecule_id': image.molecule_id,
                        'obstype': image.obstype,
                        'filter': image.filter,
-                       '@timestamp': image.dateobs,}
+                       '@timestamp': image.dateobs}
     for key, value in qc_results.items():
         # Elasticsearch does not like numpy.bool_ types
         if type(value) == np.bool_:
