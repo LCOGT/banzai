1a747e602e6201594d2eb586ac3369d8f0aae5bb
turnerm <mturner@lco.global>
2018-10-08 14:46:36 +0200

    Store missing bpm in banzai_qc

--- banzai/bpm.py
+++ banzai/bpm.py
@@ -9,6 +9,8 @@ class BPMUpdater(Stage):
 
     def do_stage(self, images):
         for image in images:
+            missing_bpm = True if not image.header['L1IDMASK'] else False
+            self.save_qc_results({'pipeline.missing_bpm': missing_bpm}, image)
             bpm_slices = array_utils.array_indices_to_slices(image.bpm)
             image.bpm[image.data[bpm_slices] >= float(image.header['SATURATE'])] = 2
         return images
