5705a4a3cc1d1bca14e2f9aaa71cd98cf3c1f08b
turnerm <mturner@lco.global>
2018-10-10 17:18:13 +0200

    Added method to search db for individual calib frames

--- banzai/dbs.py
+++ banzai/dbs.py
@@ -17,7 +17,7 @@ import requests
 from astropy.io import fits
 from sqlalchemy import create_engine, pool, desc
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, Date, Datetime, ForeignKey, Boolean, CHAR
+from sqlalchemy import Column, Integer, String, Date, DateTime, ForeignKey, Boolean, CHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import true
 
@@ -86,7 +86,7 @@ class CalibrationImageIndividual(Base):
     filename = Column(String(50), unique=True)
     filepath = Column(String(100))
     dayobs = Column(Date, index=True)
-    timestamp = Column(Datetime, index=True)
+    timestamp = Column(DateTime, index=True)
     ccdsum = Column(String(20))
     filter_name = Column(String(32))
     telescope_id = Column(Integer, ForeignKey("telescopes.id"), index=True)
@@ -460,3 +460,22 @@ def get_master_calibration_image(image, calibration_type, master_selection_crite
 
     db_session.close()
     return calibration_file
+
+
+def get_individual_calibration_images(image_parameters, db_address=_DEFAULT_DB):
+    calibration_criteria = CalibrationImageIndividual.type == image_parameters['obstype']
+    calibration_criteria &= CalibrationImage.telescope_id == image_parameters['telescope_id']
+    calibration_criteria &= CalibrationImage.dayobs == image_parameters['dayobs']
+    calibration_criteria &= CalibrationImage.ccdsum == image_parameters['ccdsum']
+    if image_parameters['obstype'] == 'SKYFLAT':
+        calibration_criteria &= CalibrationImage.filter_name == image_parameters['filter']
+
+    db_session = get_session(db_address=db_address)
+
+    calibration_images = db_session.query(CalibrationImage).filter(calibration_criteria).all()
+
+    calibration_images = [os.path.join(filepath, filename) for (filepath, filename) in zip(calibration_images.filepath,
+                                                                                     calibration_images.filename)]
+
+    db_session.close()
+    return calibration_images
