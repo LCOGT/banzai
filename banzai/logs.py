76f204a01a88157b2885fb44e74f156ae088689f
turnerm <mturner@lco.global>
2018-10-15 09:35:10 +0200

    Renamed method and added some default tags

--- banzai/logs.py
+++ banzai/logs.py
@@ -17,11 +17,11 @@ class BanzaiLogger(logging.getLoggerClass()):
         super(BanzaiLogger, self).__init__(name, level)
 
     def _log(self, level, msg, *args, **kwargs):
-        kwargs = _add_tag_dictionary(kwargs)
+        kwargs = _create_logging_tags_dictionary(kwargs)
         super(BanzaiLogger, self)._log(level, msg, *args, **kwargs)
 
 
-def _add_tag_dictionary(kwargs):
+def _create_logging_tags_dictionary(kwargs):
     tags = {}
     image = kwargs.pop('image', None)
     extra_tags = kwargs.pop('extra_tags', None)
@@ -38,7 +38,9 @@ def _image_to_tags(image_config):
             'site': image_config.site,
             'instrument': image_config.instrument,
             'epoch': date_utils.epoch_date_to_string(image_config.epoch),
-            'request_num': image_config.request_number}
+            'request_num': image_config.request_number,
+            'obstype': image_config.obstype,
+            'filter': image_config.filter}
     return tags
 
 
