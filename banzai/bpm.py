e1e0949dbe2a4ac132bb091a812c688294f406cf
turnerm <mturner@lco.global>
2018-10-16 10:14:19 +0200

    Rebased logging pr

--- banzai/bpm.py
+++ banzai/bpm.py
@@ -1,10 +1,13 @@
 import os
+import logging
 
 import numpy as np
 
 from banzai.stages import Stage
 from banzai.utils import array_utils, fits_utils
 from banzai import dbs
+
+logger = logging.getLogger(__name__)
 
 
 class BPMUpdater(Stage):
@@ -28,19 +31,19 @@ class BPMUpdater(Stage):
         self.save_qc_results({'pipeline.bpm.missing': is_bpm_missing}, image)
         if is_bpm_missing:
             # Short circuit if BPM is missing
-            self.logger.warning('Missing BPM for {0}'.format(image.filename))
+            logger.warning('Unable to find BPM in database, falling back to empty BPM', image=image)
             return
 
         # Load the BPM
-        self.logger.debug('Loading BPM {0}'.format(bpm_filename))
-        bpm = load_bpm(bpm_filename, self.logger)
+        logger.debug('Loading BPM', image=image)
+        bpm = load_bpm(bpm_filename)
 
         # Check if the BPM is the right size
         is_invalid_size = not bpm_has_valid_size(bpm, image)
         self.save_qc_results({'pipeline.bpm.invalid_size': is_invalid_size}, image)
         if is_invalid_size:
             # Short circuit if BPM is the wrong size
-            self.logger.warning('BPM shape mismatch for {0}'.format(image.filename))
+            logger.warning('BPM shape mismatch, falling back to empty BPM', image=image)
             return
 
         # Add BPM to image and header info
@@ -52,10 +55,9 @@ class BPMUpdater(Stage):
                                     db_address=self.pipeline_context.db_address)
 
 
-def load_bpm(bpm_filename, logger):
+def load_bpm(bpm_filename):
     bpm_hdu = fits_utils.open_fits_file(bpm_filename)
     bpm_extensions = fits_utils.get_extensions_by_name(bpm_hdu, 'BPM')
-    logger.info('bpm_extensions {0}'.format(len(bpm_extensions)))
     if len(bpm_extensions) > 1:
         extension_shape = bpm_extensions[0].data.shape
         bpm_shape = (len(bpm_extensions), extension_shape[0], extension_shape[1])
