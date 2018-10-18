c0536490af30f2181196dd929ba30fbd9170b5ee
turnerm <mturner@lco.global>
2018-10-18 17:31:26 +0200

    Image criteria moved to context

--- banzai/tests/test_need_to_make_preview.py
+++ banzai/tests/test_need_to_make_preview.py
@@ -1,8 +1,11 @@
+import operator
+
 import mock
+
 from banzai.dbs import TelescopeMissingException
 from banzai.preview import need_to_make_preview
 from banzai.context import TelescopeCriterion
-import operator
+from banzai.tests.utils import FakeContext
 
 md5_hash1 = '49a6bb35cdd3859224c0214310b1d9b6'
 md5_hash2 = 'aec5ef355e7e43a59fedc88ac95caed6'
@@ -23,14 +26,17 @@ class FakePreviewImage(object):
 @mock.patch('banzai.dbs.get_telescope_for_file')
 def test_no_preview_if_telescope_is_not_schedulable(mock_telescope):
     mock_telescope.return_value = FakeTelescope(schedulable=False)
-    assert not need_to_make_preview('test.fits', [TelescopeCriterion('schedulable', operator.eq, True)])
+    pipeline_context = FakeContext()
+    pipeline_context.allowed_instrument_criteria = [TelescopeCriterion('schedulable', operator.eq, True)]
+    assert not need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.dbs.get_telescope_for_file')
 def test_no_preview_if_telescope_not_in_db(mock_telescope):
     mock_telescope.return_value = FakeTelescope(schedulable=True)
     mock_telescope.side_effect = TelescopeMissingException
-    assert not need_to_make_preview('test.fits', [])
+    pipeline_context = FakeContext()
+    assert not need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.utils.file_utils.get_md5')
@@ -40,7 +46,8 @@ def test_no_preview_if_previous_success(mock_telescope, mock_preview, mock_md5):
     mock_telescope.return_value = FakeTelescope(schedulable=True)
     mock_preview.return_value = FakePreviewImage(success=True, checksum=md5_hash1)
     mock_md5.return_value = md5_hash1
-    assert not need_to_make_preview('test.fits', [])
+    pipeline_context = FakeContext()
+    assert not need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.dbs.commit_preview_image')
@@ -51,7 +58,8 @@ def test_preview_if_never_tried(mock_telescope, mock_preview, mock_md5, mock_com
     mock_telescope.return_value = FakeTelescope(schedulable=True)
     mock_preview.return_value = FakePreviewImage(success=False, checksum=md5_hash1, tries=0)
     mock_md5.return_value = md5_hash1
-    assert need_to_make_preview('test.fits', [])
+    pipeline_context = FakeContext()
+    assert need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.dbs.commit_preview_image')
@@ -62,7 +70,9 @@ def test_preview_if_tries_less_than_max(mock_telescope, mock_preview, mock_md5, 
     mock_telescope.return_value = FakeTelescope(schedulable=True)
     mock_preview.return_value = FakePreviewImage(success=False, checksum=md5_hash1, tries=3)
     mock_md5.return_value = md5_hash1
-    assert need_to_make_preview('test.fits', [], max_tries=5)
+    pipeline_context = FakeContext()
+    pipeline_context.max_preview_tries = 5
+    assert need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.dbs.commit_preview_image')
@@ -74,7 +84,9 @@ def test_no_preview_if_tries_at_max(mock_telescope, mock_preview, mock_md5, mock
     mock_telescope.return_value = FakeTelescope(schedulable=True)
     mock_preview.return_value = FakePreviewImage(success=False, checksum=md5_hash1, tries=max_tries)
     mock_md5.return_value = md5_hash1
-    assert not need_to_make_preview('test.fits', [], max_tries=max_tries)
+    pipeline_context = FakeContext()
+    pipeline_context.max_preview_tries = max_tries
+    assert not need_to_make_preview(pipeline_context)
 
 
 @mock.patch('banzai.dbs.commit_preview_image')
@@ -87,7 +99,8 @@ def test_preview_if_new_checksum(mock_telescope, mock_preview, mock_md5, mock_co
     preview_image = FakePreviewImage(success=True, checksum=md5_hash1, tries=3)
     mock_preview.return_value = preview_image
     mock_md5.return_value = md5_hash2
-    assert need_to_make_preview('test.fits', [])
+    pipeline_context = FakeContext()
+    assert need_to_make_preview(pipeline_context)
     assert not preview_image.success
     assert preview_image.tries == 0
     assert preview_image.checksum == md5_hash2
