9793c0184039edef1b8936619d72133b436af1bf
turnerm <mturner@lco.global>
2018-10-18 17:31:21 +0200

    Image criteria moved to context

--- banzai/tests/test_image_criteria.py
+++ banzai/tests/test_image_criteria.py
@@ -1,10 +1,17 @@
 from banzai.context import TelescopeCriterion
-from banzai.utils.image_utils import image_passes_criteria
 from collections import namedtuple
 import operator
 import mock
+from banzai.tests.utils import FakeContext
 
 FakeTelescope = namedtuple('FakeTelescope', ['schedulable', 'camera_type'])
+
+
+def get_fake_context(criteria=None):
+    fake_context = FakeContext()
+    if criteria is not None:
+        fake_context.allowed_instrument_criteria = criteria
+    return fake_context
 
 
 def test_telescope_criterion_should_fail():
@@ -32,7 +39,8 @@ def test_image_passes_multiple_criteria_should_fail(mock_telescope):
     mock_telescope.return_value = FakeTelescope(schedulable=False, camera_type='SciCam')
     criteria = [TelescopeCriterion('schedulable', operator.eq, True),
                 TelescopeCriterion('camera_type', operator.contains, 'NRES', exclude=True)]
-    assert not image_passes_criteria('test.fits', criteria)
+    fake_context = get_fake_context(criteria)
+    assert not fake_context.image_passes_criteria()
 
 
 @mock.patch('banzai.dbs.get_telescope_for_file')
@@ -40,7 +48,8 @@ def test_image_passes_multiple_criteria_should_pass(mock_telescope):
     mock_telescope.return_value = FakeTelescope(schedulable=True, camera_type='SciCam')
     criteria = [TelescopeCriterion('schedulable', operator.eq, True),
                 TelescopeCriterion('camera_type', operator.contains, 'NRES', exclude=True)]
-    assert image_passes_criteria('test.fits', criteria)
+    fake_context = get_fake_context(criteria)
+    assert fake_context.image_passes_criteria()
 
 
 @mock.patch('banzai.dbs.get_telescope_for_file')
@@ -48,7 +57,8 @@ def test_image_passes_multiple_criteria_should_fail_with_exclude(mock_telescope)
     mock_telescope.return_value = FakeTelescope(schedulable=True, camera_type='SciCam-NRES')
     criteria = [TelescopeCriterion('schedulable', operator.eq, True),
                 TelescopeCriterion('camera_type', operator.contains, 'NRES', exclude=True)]
-    assert not image_passes_criteria('test.fits', criteria)
+    fake_context = get_fake_context(criteria)
+    assert not fake_context.image_passes_criteria()
 
 
 @mock.patch('banzai.dbs.get_telescope_for_file')
@@ -56,4 +66,5 @@ def test_image_passes_multiple_criteria_should_pass_with_exclude(mock_telescope)
     mock_telescope.return_value = FakeTelescope(schedulable=True, camera_type='SciCam')
     criteria = [TelescopeCriterion('schedulable', operator.eq, True),
                 TelescopeCriterion('camera_type', operator.contains, 'NRES', exclude=True)]
-    assert image_passes_criteria('test.fits', criteria)
+    fake_context = get_fake_context(criteria)
+    assert fake_context.image_passes_criteria()
