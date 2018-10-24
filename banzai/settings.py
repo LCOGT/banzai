d02d7a1fe57e50c22aaa1c561fb2f0a49e79dce0
turnerm <mturner@lco.global>
2018-10-23 20:20:58 -0400

    Changed log level to be set on root

--- banzai/settings.py
+++ banzai/settings.py
@@ -7,10 +7,10 @@ logging.captureWarnings(True)
 
 # Set up the root logger
 root_logger = logging.getLogger()
-root_logger.setLevel(getattr(logging, 'DEBUG'))
 root_handler = logging.StreamHandler(sys.stdout)
 
 # Add handler
 formatter = LCOGTFormatter()
 root_handler.setFormatter(formatter)
+root_handler.setLevel(getattr(logging, 'DEBUG'))
 root_logger.addHandler(root_handler)
