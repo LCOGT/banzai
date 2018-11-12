44019385b5c793a234747db28da63c20919740fd
turnerm <mturner@lco.global>
2018-11-12 15:51:09 +0100

    Moved things around, added more log messages

--- banzai/utils/image_utils.py
+++ banzai/utils/image_utils.py
@@ -23,62 +23,78 @@ def image_passes_criteria(filename, criteria, db_address=dbs._DEFAULT_DB):
     return passes
 
 
-def _image_is_correct_obstype(filename, image_types):
-    passes = False
+def image_is_correct_obstype(filename, image_types):
     obstype = None
     hdu_list = fits.open(filename)
     for hdu in hdu_list:
         if 'OBSTYPE' in hdu.header.keys():
             obstype = hdu.header['OBSTYPE']
+    passes = True if obstype in image_types else False
     if obstype is None:
         logger.error('Unable to get OBSTYPE', extra={'tags': {'filename': filename}})
-        passes = False
-    elif obstype in image_types:
-        passes = True
     return passes
 
 
-def _get_calibration_image_parameters(filename, db_address):
+def select_images(image_path_list, image_types, instrument_criteria, db_address=dbs._DEFAULT_DB):
+    logger.info("Selecting images to reduce from directory image path list")
+    pruned_image_path_list = []
+    for filename in image_path_list:
+        try:
+            if image_passes_criteria(filename, instrument_criteria, db_address=db_address) and \
+               image_is_correct_obstype(filename, image_types):
+                pruned_image_path_list.append(filename)
+        except Exception as e:
+            logger.error('Exception checking image selection criteria: {e}'.format(e=e),
+                         extra_tags={'filename': filename})
+    logger.debug("Found {n_images} images to reduce".format(n_images=len(pruned_image_path_list)))
+    return pruned_image_path_list
+
+
+def get_calibration_image_parameters(filename, group_by_attributes, db_address):
     telescope = dbs.get_telescope_for_file(filename, db_address=db_address)
     _, header, _, _ = fits_utils.open_image(filename)
     image_parameters = {
-        'ccdsum': header['CCDSUM'],
-        'filter': header['FILTER'],
         'dayobs': header['DAY-OBS'],
         'obstype': header['OBSTYPE'],
         'telescope_id': telescope.id}
+    for attribute in group_by_attributes:
+        # Works for CCDSUM and FILTER
+        image_parameters[attribute] = header[attribute.upper()]
     return image_parameters
 
 
-def select_calibration_images(image_list, image_types, instrument_criteria, db_address=dbs._DEFAULT_DB):
-    images = []
+def select_calibration_images(image_path_list, image_types, instrument_criteria, group_by_attributes,
+                              db_address=dbs._DEFAULT_DB):
+    logger.info("Selecting individual reduced calibration images from database")
+    # Using the raw image path list, check that each image file passes the criteria and is the correct obstype.
+    # If so, record the parameters needed to query the db if they don't already exist in the list.
     image_parameters_list = []
-    for filename in image_list:
+    for filename in image_path_list:
         try:
             if image_passes_criteria(filename, instrument_criteria, db_address=db_address) and \
-               _image_is_correct_obstype(filename, image_types):
-                image_parameters = _get_calibration_image_parameters(filename, db_address=db_address)
+               image_is_correct_obstype(filename, image_types):
+                image_parameters = get_calibration_image_parameters(filename, group_by_attributes,
+                                                                    db_address=db_address)
                 if image_parameters not in image_parameters_list:
                     image_parameters_list.append(image_parameters)
         except Exception as e:
             logger.error('Exception checking image selection criteria: {e}'.format(e=e),
                          extra={'tags': {'filename': filename}})
+    logger.debug("Checked {n_images} images, and found {n_parameter_sets} sets of parameters".format(
+        n_images=len(image_path_list), n_parameter_sets=len(image_parameters_list)))
+    # For each unique set of image parameters, query the db for reduced image files that match.
+    # Return a list of reduced image filenames for each set of image parameters.
+    calibration_image_path_lists = []
     for image_parameters in image_parameters_list:
-        images.extend(dbs.get_individual_calibration_images(image_parameters, db_address=db_address))
-    return images
-
-
-def select_images(image_list, image_types, instrument_criteria, db_address=dbs._DEFAULT_DB):
-    images = []
-    for filename in image_list:
-        try:
-            if image_passes_criteria(filename, instrument_criteria, db_address=db_address) and \
-               _image_is_correct_obstype(filename, image_types):
-                images.append(filename)
-        except Exception as e:
-            logger.error('Exception checking image selection criteria: {e}'.format(e=e),
-                         extra_tags={'filename': filename})
-    return images
+        calibration_image_path_lists.append(
+            dbs.get_individual_calibration_images(image_parameters,
+                                                  group_by_attributes=group_by_attributes,
+                                                  db_address=db_address))
+    logger.debug("Returning {n_lists} image path lists, with respective lengths {len_lists}".format(
+        n_lists=len(calibration_image_path_lists),
+        len_lists=[len(image_path_list) for image_path_list in calibration_image_path_lists]
+    ))
+    return calibration_image_path_lists
 
 
 def make_image_path_list(raw_path):
