38e8174ffcdf5e3996bbacdacd3220c02f8f154c
Matt Daily <mdaily@lco.global>
2019-01-30 09:38:43 -0800

    Use GAIA Astrometry service for WCS solutions.

--- banzai/astrometry.py
+++ banzai/astrometry.py
@@ -3,6 +3,7 @@ import subprocess
 import shlex
 import tempfile
 import logging
+import requests
 
 from astropy.io import fits
 from astropy.wcs import WCS
@@ -15,75 +16,55 @@ from banzai.utils import image_utils
 
 logger = logging.getLogger(__name__)
 
+_ASTROMETRY_SERVICE_URL='http://astrometry.lco.gtn/catalog/'
 
 class WCSSolver(Stage):
-    cmd = 'solve-field --crpix-center --no-verify --no-tweak ' \
-          ' --radius 2.0 --ra {ra} --dec {dec} --guess-scale ' \
-          '--scale-units arcsecperpix --scale-low {scale_low} --scale-high {scale_high} ' \
-          '--no-plots -N none --no-remove-lines ' \
-          '--code-tolerance 0.003 --pixel-error 1 -d 1-200 ' \
-          '--solved none --match none --rdls none --wcs {wcs_name} --corr none --overwrite ' \
-          '-X X -Y Y -s FLUX --width {nx} --height {ny} {catalog_name}'
 
     def __init__(self, pipeline_context):
         super(WCSSolver, self).__init__(pipeline_context)
 
     def do_stage(self, images):
 
-        for i, image in enumerate(images):
+        for image in images:
 
             # Skip the image if we don't have some kind of initial RA and Dec guess
             if np.isnan(image.ra) or np.isnan(image.dec):
                 logger.error('Skipping WCS solution. No initial pointing guess from header.', image=image)
                 continue
 
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                filename = os.path.basename(image.filename)
-                catalog_name = os.path.join(tmpdirname, filename.replace('.fits', '.cat.fits'))
-                try:
-                    image.write_catalog(catalog_name, nsources=40)
-                except image_utils.MissingCatalogException:
-                    image.header['WCSERR'] = (4, 'Error status of WCS fit. 0 for no error')
-                    logger.error('No source catalog. Not attempting WCS solution', image=image)
-                    continue
+            image_catalog = image.data_tables.get('catalog')
 
-                # Run astrometry.net
-                wcs_name = os.path.join(tmpdirname, filename.replace('.fits', '.wcs.fits'))
-                command = self.cmd.format(ra=image.ra, dec=image.dec, scale_low=0.9*image.pixel_scale,
-                                          scale_high=1.1*image.pixel_scale, wcs_name=wcs_name,
-                                          catalog_name=catalog_name, nx=image.nx, ny=image.ny)
+            catalog_payload = {'X': list(image_catalog['x']),
+                               'Y': list(image_catalog['y']),
+                               'FLUX': list(image_catalog['flux']),
+                               'pixel_scale': image.pixel_scale,
+                               'naxis': 2,
+                               'naxis1': image.nx,
+                               'naxis2': image.ny,
+                               'ra': image.ra,
+                               'dec': image.dec}
+            try:
+                astrometry_response = requests.post(_ASTROMETRY_SERVICE_URL, json=catalog_payload)
+                astrometry_response.raise_for_status()
+            except HTTPError:
+                logger.error('Error from Astrometry service. WCS solution not completed.', image=image)
+                continue
 
-                try:
-                    console_output = subprocess.check_output(shlex.split(command))
-                except subprocess.CalledProcessError:
-                    logger.error('Astrometry.net threw an error.', image=image)
+            header_keywords_to_update = ['CTYPE1', 'CTYPE2', 'CRPIX1', 'CRPIX2', 'CRVAL1',
+                                         'CRVAL2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2']
 
-                logger.debug(console_output, image=image)
+            for keyword in header_keywords_to_update:
+                image.header[keyword] = astrometry_response.json()[keyword]
 
-                if os.path.exists(wcs_name):
-                    # Copy the WCS keywords into original image
-                    new_header = fits.getheader(wcs_name)
+            image.header['RA'], image.header['DEC'] = get_ra_dec_in_sexagesimal(image.header['CRVAL1'],
+                                                                                image.header['CRVAL2'])
 
-                    header_keywords_to_update = ['CTYPE1', 'CTYPE2', 'CRPIX1', 'CRPIX2', 'CRVAL1',
-                                                 'CRVAL2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2']
-                    for keyword in header_keywords_to_update:
-                        image.header[keyword] = new_header[keyword]
+            add_ra_dec_to_catalog(image)
 
-                    image.header['WCSERR'] = (0, 'Error status of WCS fit. 0 for no error')
-
-                    # Update the RA and Dec header keywords
-                    image.header['RA'], image.header['DEC'] = get_ra_dec_in_sexagesimal(image.header['CRVAL1'],
-                                                                                        image.header['CRVAL2'])
-
-                    # Clean up wcs file
-                    os.remove(wcs_name)
-
-                    # Add the RA and Dec values to the catalog
-                    add_ra_dec_to_catalog(image)
-                else:
-                    image.header['WCSERR'] = (4, 'Error status of WCS fit. 0 for no error')
+            image.header['WCSERR'] = (0, 'Error status of WCS fit. 0 for no error')
 
             logger.info('Attempted WCS Solve', image=image, extra_tags={'WCSERR': image.header['WCSERR']})
+
         return images
 
 
