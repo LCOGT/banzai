c6a9bfbf0606d74137ff16ca034d088b2fdafdb6
turnerm <mturner@lco.global>
2019-03-18 11:02:54 +0100

    First commit

---
+++ banzai/qc/zeros.py
@@ -0,0 +1,41 @@
+import logging
+
+import numpy as np
+
+from banzai.stages import Stage
+from banzai.utils import qc
+
+logger = logging.getLogger(__name__)
+
+
+class ZerosTest(Stage):
+    """
+    Reject any images that have ZEROS_THRESHOLD or more of their pixels exactly equal to 0.
+
+    Notes
+    =====
+    Sometimes when a camera fails, all pixels have a value of 0.
+    """
+    ZEROS_THRESHOLD = 0.2
+
+    def __init__(self, pipeline_context):
+        super(ZerosTest, self).__init__(pipeline_context)
+
+    def do_stage(self, image):
+        npixels = np.product(image.data.shape)
+        fraction_0s = float(np.sum(image.data == 0)) / npixels
+        logging_tags = {'FRAC0': fraction_0s,
+                        'threshold': self.ZEROS_THRESHOLD}
+        has_0s_error = fraction_0s > self.ZEROS_THRESHOLD
+        qc_results = {'zeros_test.failed': has_0s_error,
+                      'zeros_test.fraction': fraction_0s,
+                      'zeros_testthreshold': self.ZEROS_THRESHOLD}
+        if has_0s_error:
+            logger.error('Image is mostly 0s. Rejecting image', image=image, extra_tags=logging_tags)
+            qc_results['rejected'] = True
+            return None
+        else:
+            logger.info('Measuring fraction of 0s.', image=image, extra_tags=logging_tags)
+        qc.save_qc_results(self.pipeline_context, qc_results, image)
+
+        return image
---
+++ banzai/tests/test_zeros_qc.py
@@ -0,0 +1,65 @@
+import pytest
+import numpy as np
+
+from banzai.tests.utils import FakeImage
+from banzai.qc import ZerosTest
+
+
+@pytest.fixture(scope='module')
+def set_random_seed():
+    np.random.seed(81232385)
+
+
+def test_null_input_image():
+    tester = ZerosTest(None)
+    image = tester.run(None)
+    assert image is None
+
+
+def test_no_pixels_0():
+    tester = ZerosTest(None)
+    image = tester.do_stage(FakeImage())
+    assert image is not None
+
+
+def test_nonzero_but_no_pixels_0():
+    tester = ZerosTest(None)
+    image = FakeImage()
+    image.data += 5
+    image = tester.do_stage(image)
+    assert image is not None
+
+
+def test_image_all_0s():
+    tester = ZerosTest(None)
+    image = FakeImage()
+    image.data += 5
+    image.data[:, :] = 0
+    image = tester.do_stage(image)
+    assert image is None
+
+
+def test_image_5_percent_0(set_random_seed):
+    tester = ZerosTest(None)
+    nx = 101
+    ny = 103
+    image = FakeImage(nx=nx, ny=ny)
+    random_pixels_x = np.random.randint(0, nx - 1, size=int(0.05 * nx * ny))
+    random_pixels_y = np.random.randint(0, ny - 1, size=int(0.05 * nx * ny))
+    for i in zip(random_pixels_y, random_pixels_x):
+        image.data[i] = 0
+    image = tester.do_stage(image)
+    assert image is not None
+
+
+def test_image_30_percent_0(set_random_seed):
+    tester = ZerosTest(None)
+    nx = 101
+    ny = 103
+    image = FakeImage(nx=nx, ny=ny)
+    random_pixels_x = np.random.randint(0, nx - 1, size=int(0.3 * nx * ny))
+    random_pixels_y = np.random.randint(0, ny - 1, size=int(0.3 * nx * ny))
+    for i in zip(random_pixels_y, random_pixels_x):
+        image.data[i] = 0
+    image = tester.do_stage(image)
+    assert image is None
