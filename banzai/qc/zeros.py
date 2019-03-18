3d1111fc6d3cc7d5ce843acb909404dedbe46e17
turnerm <mturner@lco.global>
2019-03-18 14:31:31 +0100

    Zeros test on individual amps

--- banzai/qc/zeros.py
+++ banzai/qc/zeros.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class ZerosTest(Stage):
     """
-    Reject any images that have ZEROS_THRESHOLD or more of their pixels exactly equal to 0.
+    Reject any images that have ZEROS_THRESHOLD or more of their pixels in a single amp exactly equal to 0.
 
     Notes
     =====
@@ -22,14 +22,17 @@ class ZerosTest(Stage):
         super(ZerosTest, self).__init__(pipeline_context)
 
     def do_stage(self, image):
-        npixels = np.product(image.data.shape)
-        fraction_0s = float(np.sum(image.data == 0)) / npixels
-        logging_tags = {'FRAC0': fraction_0s,
+        fraction_0s_list = []
+        for i_amp in range(image.get_n_amps()):
+            npixels = np.product(image.data[i_amp].shape)
+            fraction_0s_list.append(float(np.sum(image.data[i_amp] == 0)) / npixels)
+
+        has_0s_error = any([fraction_0s > self.ZEROS_THRESHOLD for fraction_0s in fraction_0s_list])
+        logging_tags = {'FRAC0': fraction_0s_list,
                         'threshold': self.ZEROS_THRESHOLD}
-        has_0s_error = fraction_0s > self.ZEROS_THRESHOLD
         qc_results = {'zeros_test.failed': has_0s_error,
-                      'zeros_test.fraction': fraction_0s,
-                      'zeros_testthreshold': self.ZEROS_THRESHOLD}
+                      'zeros_test.fraction': fraction_0s_list,
+                      'zeros_test.threshold': self.ZEROS_THRESHOLD}
         if has_0s_error:
             logger.error('Image is mostly 0s. Rejecting image', image=image, extra_tags=logging_tags)
             qc_results['rejected'] = True
--- banzai/tests/test_zeros_qc.py
+++ banzai/tests/test_zeros_qc.py
@@ -10,43 +10,62 @@ def set_random_seed():
     np.random.seed(81232385)
 
 
+def get_random_pixel_pairs(nx, ny, fraction):
+    return np.unravel_index(np.random.choice(range(nx * ny), size=int(fraction * nx * ny)), (ny, nx))
+
+
 def test_null_input_image():
     tester = ZerosTest(None)
     image = tester.run(None)
     assert image is None
 
 
-def test_no_pixels_0():
+def test_no_pixels_0_not_rejected():
     tester = ZerosTest(None)
-    image = tester.do_stage(FakeImage())
+    image = tester.do_stage(FakeImage(image_multiplier=5))
     assert image is not None
 
 
-def test_nonzero_but_no_pixels_0():
+def test_image_all_0s_rejected():
     tester = ZerosTest(None)
     image = FakeImage()
-    image.data += 5
-    image = tester.do_stage(image)
-    assert image is not None
-
-
-def test_image_all_0s():
-    tester = ZerosTest(None)
-    image = FakeImage()
-    image.data += 5
     image.data[:, :] = 0
     image = tester.do_stage(image)
     assert image is None
 
 
-def test_image_95_percent_0(set_random_seed):
+def test_image_50_percent_0_not_rejected(set_random_seed):
     tester = ZerosTest(None)
     nx = 101
     ny = 103
     image = FakeImage(nx=nx, ny=ny)
-    random_pixels_x = np.random.randint(0, nx - 1, size=int(0.95 * nx * ny))
-    random_pixels_y = np.random.randint(0, ny - 1, size=int(0.95 * nx * ny))
-    for i in zip(random_pixels_y, random_pixels_x):
-        image.data[i] = 0
+    random_pixels = get_random_pixel_pairs(nx, ny, 0.5)
+    for j,i in zip(random_pixels[0], random_pixels[1]):
+        image.data[i,i] = 0
     image = tester.do_stage(image)
     assert image is not None
+
+
+def test_image_99_percent_0_rejected(set_random_seed):
+    tester = ZerosTest(None)
+    nx = 101
+    ny = 103
+    image = FakeImage(nx=nx, ny=ny)
+    random_pixels = get_random_pixel_pairs(nx, ny, 0.99)
+    for j,i in zip(random_pixels[0], random_pixels[1]):
+        image.data[i] = 0
+    image = tester.do_stage(image)
+    assert image is None
+
+
+def test_single_amp_99_percent_0_rejected(set_random_seed):
+    tester = ZerosTest(None)
+    nx = 101
+    ny = 103
+    n_amps = 4
+    image = FakeImage(nx=nx, ny=ny, n_amps=n_amps)
+    random_pixels = get_random_pixel_pairs(nx, ny, 0.99)
+    for j,i in zip(random_pixels[0], random_pixels[1]):
+        image.data[0][i] = 0
+    image = tester.do_stage(image)
+    assert image is None
