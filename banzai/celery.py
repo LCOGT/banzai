df9eac409aab491dfae4a90a9af995957018afbf
David Collom <dmcollom@gmail.com>
2019-05-02 14:16:25 -0700

    Adding broker url to runtime context

--- banzai/celery.py
+++ banzai/celery.py
@@ -61,7 +61,7 @@ def schedule_stack(self, runtime_context_json, blocks, process_any_images=True):
 
 @app.task(name='celery.process_image')
 def process_image(path, runtime_context_dict):
-    logger.info('Got into actor.')
+    logger.info('Running process image.')
     runtime_context = Context(runtime_context_dict)
     try:
         if realtime_utils.need_to_process_image(path, runtime_context,
@@ -109,6 +109,6 @@ def schedule_stacking_checks(runtime_context):
                 message_delay_in_seconds = 0  # Remove delay if block end is in the past
             else:
                 message_delay_in_seconds = message_delay.seconds
-            logger.info('scheduling with message delay {0}'.format(str(message_delay_in_seconds)))
+            logger.info('Scheduling stacking for block type {0} with message delay {1}'.format(worker_runtime_context['frame_type'], str(message_delay_in_seconds)))
             schedule_stack.apply_async(args=(worker_runtime_context, blocks_for_calibration),
                                        countdown=message_delay_in_seconds)
