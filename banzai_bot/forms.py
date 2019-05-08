82d4da0a230d4a263a75dfdcd7ed637209695014
Matt Daily <mdaily@lco.global>
2019-05-08 15:32:29 -0700

    Add ReprocessDayObsForm for re-processing frames for a range of dates

--- banzai_bot/forms.py
+++ banzai_bot/forms.py
@@ -2,7 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, Selectfield 
 
 
-
 class ReprocessDayObsForm(FlaskForm):
-    dayobs = DateField('Day-Obs', validators=[DataRequired()])
+    dayobs_start = DateField('Day-Obs Start', validators=[DataRequired()])
+    dayobs_end = DateField('Day-Obs End', validators=[DataRequired())
+    site = SelectField('Site Code', validators=[DataRequired()])
+    instrument = SelectField('Instrument Code', validators=[DataRequired()])
+
 
