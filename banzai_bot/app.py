497854351054a3de04bd0e9a29c41bb9520530d7
Valentina Hallefors <vhallefors@lco.global>
2019-05-09 09:11:55 -0700

    Change name to BANZAI Tools

--- banzai_bot/app.py
+++ banzai_bot/app.py
@@ -1,15 +1,10 @@
 from flask import Flask, render_template
 import requests
-<<<<<<< HEAD
 from banzai_bot.utils import get_sites_and_instruments
 from banzai_bot.forms import ReprocessDayObsForm
 
-=======
-from banzai_bot import utils
-from forms import ReprocessDayObsForm
->>>>>>> d7972fc88fce91f39b18341410ebd544dd0a63f1
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -18,20 +13,10 @@ def index():
 
 @app.route('/reprocess_dayobs')
 def reprocess_dayobs():
-<<<<<<< HEAD
     form = ReprocessDayObsForm()
-    sites, instruments = get_sites_and_instruments()
-    form.sites.choices = sites
-    form.instruments.choices = instruments
+    # sites, instruments = get_sites_and_instruments()
+    sites = ['tfn', 'ogg']
+    instruments = ['kb95', 'fa06']
+    form.site.choices = sites
+    form.instrument.choices = instruments
     return render_template('reprocess_dayobs.html', title='Reprocess Dayobs', form=form)
-=======
-    sites, instruments = utils.get_sites_and_instruments()
-    form = ReprocessDayObsForm()
-    form.sites.choices = sites
-    form.instruments.choices = instruments
-
-
-
-    
-
->>>>>>> d7972fc88fce91f39b18341410ebd544dd0a63f1
