965aae6f17188adeeae19c32517ec6cbdc994946
Matt Daily <mdaily@lco.global>
2019-05-09 10:00:24 -0700

    Plumb cameras and instruments to frontend. Rename project directory to banzai_tools

--- banzai_tools/app.py
+++ banzai_tools/app.py
@@ -5,7 +5,6 @@ from banzai_bot.forms import ReprocessDayObsForm
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,8 +14,11 @@ def index():
 def reprocess_dayobs():
     form = ReprocessDayObsForm()
     sites, instruments = get_sites_and_instruments()
-    sites = ['tfn', 'ogg']
-    instruments = ['kb95', 'fa06']
-    form.site.choices = sites
-    form.instrument.choices = instruments
+
+    sites_display = [(site, site) for site in sites]
+    instruments_display = [(instrument, instrument) for instrument in instruments]
+    form.site.choices = sites_display
+    form.instrument.choices = instruments_display
     return render_template('reprocess_dayobs.html', title='Reprocess Dayobs', form=form)
+
+    #after submission, we want to call a script that will execute on chanunpa
