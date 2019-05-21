d0a43ee82d5a414cfcb49f51597a2602069cb73d
Curtis McCully <cmccully@lco.global>
2019-05-21 20:40:46 +0000

    Made the datetime serialization consistent between celery and the console entry points.

--- banzai/dbs.py
+++ banzai/dbs.py
@@ -453,14 +453,14 @@ def get_master_calibration_image(image, calibration_type, master_selection_crite
     return calibration_file
 
 
-def get_individual_calibration_images(instrument, calibration_type, min_date, max_date,
-                                      use_masters=False, db_address=_DEFAULT_DB):
+def get_individual_calibration_images(instrument, calibration_type, min_date: datetime.datetime,
+                                      max_date: datetime.datetime, use_masters=False, db_address=_DEFAULT_DB):
 
     calibration_criteria = CalibrationImage.instrument_id == instrument.id
     calibration_criteria &= CalibrationImage.type == calibration_type.upper()
     calibration_criteria &= CalibrationImage.is_master.is_(use_masters)
-    calibration_criteria &= CalibrationImage.dateobs >= datetime.datetime.strptime(min_date, date_utils.TIMESTAMP_FORMAT)
-    calibration_criteria &= CalibrationImage.dateobs <= datetime.datetime.strptime(max_date, date_utils.TIMESTAMP_FORMAT)
+    calibration_criteria &= CalibrationImage.dateobs >= min_date
+    calibration_criteria &= CalibrationImage.dateobs <= max_date
 
     with get_session(db_address=db_address) as db_session:
         images = db_session.query(CalibrationImage).filter(calibration_criteria).all()
