685437a1f7edf21d52b677bf24f9f4612184b762
Curtis McCully <cmccully@lco.global>
2019-05-21 23:51:30 +0000

    Another attempt to override celery logger.

--- banzai/celeryconfig.py
+++ banzai/celeryconfig.py
@@ -2,4 +2,3 @@ broker_url = 'redis://localhost:6379/0'
 imports = ('banzai.main', 'banzai.celery',)
 worker_prefetch_multiplier = 1
 worker_max_tasks_per_child = 100
-worker_hijack_root_logger = False
