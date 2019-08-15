8293289c38ec4ce1a502d2c0d109ebf27f32175a
Ara Ye <aye@lco.global>
2019-08-15 13:40:12 -0700

    more logging

--- banzai/calibrations.py
+++ banzai/calibrations.py
@@ -81,6 +81,7 @@ class CalibrationStacker(CalibrationMaker):
             # del image.bpm
             logger.info(psutil.virtual_memory())
 
+        logger.debug('^^ Mean')
         stacked_data = stats.sigma_clipped_mean(data_stack, 3.0, axis=2, mask=stack_mask, inplace=True)
 
         # Memory cleanup
