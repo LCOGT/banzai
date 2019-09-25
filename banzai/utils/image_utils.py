a6ce30059ac3e7f155e6d7169a7008d91ba5d4cf
Curtis McCully <cmccully@lco.global>
2019-09-25 20:47:45 +0000

    WIP. Not stable.

--- banzai/utils/image_utils.py
+++ banzai/utils/image_utils.py
@@ -1,29 +1,19 @@
 import logging
 
-from banzai import dbs
 from banzai.utils.instrument_utils import instrument_passes_criteria
 
 
 logger = logging.getLogger('banzai')
 
 
-def image_can_be_processed(header, context, filename):
-    if header is None:
-        logger.warning('Header being checked to process image is None', extra_tags={'filename': filename})
-        return False
+def image_can_be_processed(image, context):
     # Short circuit if the instrument is a guider even if they don't exist in configdb
-    if not header.get('OBSTYPE') in context.LAST_STAGE:
-        logger.warning('Image has an obstype that is not supported by banzai.', extra_tags={'filename': filename})
+    if image.observation_type not in context.LAST_STAGE:
+        logger.warning('Image has an obstype that is not supported by banzai.', extra_tags={'filename': image.filename})
         return False
-    try:
-        instrument = dbs.get_instrument(header, db_address=context.db_address, configdb_address=context.CONFIGDB_URL)
-    except ValueError:
-        return False
-    passes = instrument_passes_criteria(instrument, context.FRAME_SELECTION_CRITERIA)
+    passes = instrument_passes_criteria(image.instrument, context.FRAME_SELECTION_CRITERIA)
     if not passes:
-        logger.debug('Image does not pass reduction criteria', extra_tags={'filename': filename})
-    reduction_level = header.get('RLEVEL', '00')
-    passes &= reduction_level == '00'
-    if reduction_level != '00':
-        logger.debug('Image has nonzero reduction level', extra_tags={'filename': filename})
+        logger.debug('Image does not pass reduction criteria', extra_tags={'filename': image.filename})
+    if not image.is_raw():
+        logger.debug('Image is now raw. It has already been processed', extra_tags={'filename': image.filename})
     return passes
