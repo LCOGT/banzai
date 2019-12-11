5d1d4cf0cd0a9ca86be056734b207b4de1868887
Curtis McCully <cmccully@lco.global>
2019-12-11 00:15:00 +0000

    Updated image can be processed log message.

--- banzai/utils/image_utils.py
+++ banzai/utils/image_utils.py
@@ -9,7 +9,7 @@ logger = logging.getLogger('banzai')
 def image_can_be_processed(image, context):
     # Short circuit if the instrument is a guider even if they don't exist in configdb
     if image.obstype not in context.SUPPORTED_FRAME_TYPES:
-        logger.debug('Image has an obstype that is not supported by banzai.', extra_tags={'filename': image.filename})
+        logger.debug('Image has an obstype that is not supported.', extra_tags={'filename': image.filename})
         return False
     passes = instrument_passes_criteria(image.instrument, context.FRAME_SELECTION_CRITERIA)
     if not passes:
