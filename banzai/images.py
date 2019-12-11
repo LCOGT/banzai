0b0871bd38b8d1e058c1ea1c9a3d2da089ccd46b
Curtis McCully <cmccully@lco.global>
2019-12-11 00:58:14 +0000

    Bugfix in filtering images.

--- banzai/images.py
+++ banzai/images.py
@@ -786,6 +786,8 @@ class LCOFrameFactory:
             image = cls.observation_frame_class(hdu_list, os.path.basename(path))
         image.instrument = cls._get_instrument(image, runtime_context.db_address)
 
+        if image.instrument is None:
+            return
         # TODO: Put all munge code here
 
         for hdu in image.ccd_hdus:
@@ -821,7 +823,7 @@ class LCOFrameFactory:
                 binning = hdu.meta.get('CCDSUM', '1 1')
                 n_binned_pixels = int(binning[0]) * int(binning[2])
                 update_saturate(image, hdu, 125000.0 * n_binned_pixels / float(hdu.meta['GAIN']))
-        if image.instrument is not None and image_utils.image_can_be_processed(image, runtime_context):
+        if image_utils.image_can_be_processed(image, runtime_context):
             return image
         else:
             return None
