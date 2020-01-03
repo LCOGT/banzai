8aac6adc2c6d59b67328b890c628e58d6487050b
Matt Daily <mdaily@lco.global>
2020-01-03 14:04:38 -0800

    Update crosstalk corrector stage. Fix crosstalk corrector tests.

--- banzai/crosstalk.py
+++ banzai/crosstalk.py
@@ -12,20 +12,20 @@ class CrosstalkCorrector(Stage):
         super(CrosstalkCorrector, self).__init__(runtime_context)
 
     def do_stage(self, image):
-        if image.data_is_3d():
+        n_amps = len(image.ccd_hdus)
+        if n_amps > 1:
             logging_tags = {}
-            n_amps = image.get_n_amps()
             crosstalk_matrix = np.identity(n_amps)
             for j in range(n_amps):
                 for i in range(n_amps):
                     if i != j:
                         crosstalk_keyword = 'CRSTLK{0}{1}'.format(i + 1, j + 1)
-                        crosstalk_matrix[i, j] = -float(image.header[crosstalk_keyword])
-                        logging_tags[crosstalk_keyword] = image.header[crosstalk_keyword]
+                        crosstalk_matrix[i, j] = -float(image.meta[crosstalk_keyword])
+                        logging_tags[crosstalk_keyword] = image.meta[crosstalk_keyword]
             logger.info('Removing crosstalk', image=image, extra_tags=logging_tags)
-            # Techinally, we should iterate this process because crosstalk doesn't
+            # Technically, we should iterate this process because crosstalk doesn't
             # produce more crosstalk
-            """This dot product is effectivly the following:
+            """This dot product is effectively the following:
             coeffs = [[Q11, Q12, Q13, Q14],
                       [Q21, Q22, Q23, Q24],
                       [Q31, Q32, Q33, Q34],
@@ -37,5 +37,7 @@ class CrosstalkCorrector(Stage):
             D3 = D3 - Q13 D1 - Q23 D2 - Q43 D4
             D4 = D4 - Q14 D1 - Q24 D2 - Q34 D3
             """
-            image.data = np.dot(crosstalk_matrix.T, np.swapaxes(image.data, 0, 1))
+            corrected_data = np.dot(crosstalk_matrix.T, np.swapaxes([extension.data for extension in image.ccd_hdus], 0, 1))
+            for extension, corrected in zip(image.ccd_hdus, corrected_data):
+                extension.data = corrected
         return image
