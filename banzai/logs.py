3448c02b569bdc8d8bbbbae45f72941a57602410
Curtis McCully <cmccully@lco.global>
2020-05-08 15:08:43 -0700

    Set default logger level to INFO so that more logs are printed.

--- banzai/logs.py
+++ banzai/logs.py
@@ -8,7 +8,7 @@ from banzai.utils import date_utils
 
 
 class BanzaiLogger(logging.getLoggerClass()):
-    def __init__(self, name, level='NOTSET'):
+    def __init__(self, name, level='INFO'):
         super(BanzaiLogger, self).__init__(name, level)
 
     def _log(self, level, msg, *args, **kwargs):
