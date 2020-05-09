b8426f7aa12f76910fd2ee5b4c4c873f3b49618d
Matt Daily <matt.daily85@gmail.com>
2020-05-08 17:26:48 -0700

    Remove need to pass in OBSTYPE. Handle obstype selection at upper level, and pass list of ordered extensions.

--- banzai/utils/fits_utils.py
+++ banzai/utils/fits_utils.py
@@ -216,15 +216,13 @@ def to_fits_image_extension(data, master_extension_name, extension_name, context
     return fits.ImageHDU(data=data, header=header)
 
 
-def reorder_hdus(hdu_list: fits.HDUList,  obstype: str, ordering_dict: dict):
+def reorder_hdus(hdu_list: fits.HDUList, extensions: list):
     """
     Re-order HDUs in an HDUList before writing to disk
     :param hdu_list: Astropy fits.HDUList
-    :param obstype: Observation type from header
-    :param ordering_dict: A dictionary, keyed by OBSTYPE with the desired ordering of
-    extensions by EXTNAME
+    :param extensions: Ordered list of extensions by EXTNAME
     """
-    for idx, extension_name in enumerate(ordering_dict.get(obstype)):
+    for idx, extension_name in enumerate(extensions):
         if hdu_list[idx].name != extension_name:
             hdu = hdu_list[extension_name]
             hdu_list.remove(hdu)
