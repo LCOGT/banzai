05b8bc3a7f9aef39defddb53ea3fc99b3dcfb885
Matt Daily <mgdaily@users.noreply.github.com>
2023-06-27 16:17:13 +0000

    Rename temperature variable for clarity
    temp is a very overloaded name in CS

--- banzai/dark.py
+++ banzai/dark.py
@@ -43,14 +43,14 @@ class DarkSubtractor(CalibrationUser):
 
     def apply_master_calibration(self, image, master_calibration_image):
         master_calibration_image *= image.exptime
-        temp_scaling_factor = np.exp(master_calibration_image.dark_temperature_coefficient * \
-                                     (image.measured_ccd_temperature - master_calibration_image.measured_ccd_temperature))
-        master_calibration_image *= temp_scaling_factor
+        temperature_scaling_factor = np.exp(master_calibration_image.dark_temperature_coefficient * \
+                                            (image.measured_ccd_temperature - master_calibration_image.measured_ccd_temperature))
+        master_calibration_image *= temperature_scaling_factor
 
         image -= master_calibration_image
         image.meta['L1IDDARK'] = master_calibration_image.filename, 'ID of dark frame'
         image.meta['L1STATDA'] = 1, 'Status flag for dark frame correction'
-        image.meta['DRKTSCAL'] = temp_scaling_factor, 'Temperature scaling factor applied to dark image'
+        image.meta['DRKTSCAL'] = temperature_scaling_factor, 'Temperature scaling factor applied to dark image'
         return image
 
 
