fde6732130fe40548185370971679c4427742eb7
Curtis McCully <cmccully@lco.global>
2025-02-03 13:59:59 -0500

    Added alembic migration for the database.

---
+++ alembic/migrations/env.py
@@ -0,0 +1,47 @@
+from logging.config import fileConfig
+
+import os
+from sqlalchemy import pool, create_engine
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = None
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connection_url = os.getenv("DATABASE_URL", "sqlite:///fallback.db")
+    connectable = create_engine(connection_url, poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+run_migrations()
