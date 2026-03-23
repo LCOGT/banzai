1d24f666cbe93b6808ecd5d6a2d05b7ac4371cb6
Tim Beccue <timothybeccue@gmail.com>
2026-03-23 14:40:48 -0300

    Handle existing replication slot when recreating subscription

--- banzai/tests/test_replication.py
+++ banzai/tests/test_replication.py
@@ -1,5 +1,7 @@
 import mock
 import pytest
+from psycopg2 import errors as pg_errors
+from sqlalchemy.exc import OperationalError
 from sqlalchemy.sql.elements import TextClause
 
 from banzai.cache import replication
@@ -30,10 +32,32 @@ class TestSetupSubscription:
         assert 'banzai_lsc_sub' in sql_arg.text
         assert 'banzai_lsc_slot' in sql_arg.text
         assert 'banzai_calibrations' in sql_arg.text
+        assert 'create_slot = true' in sql_arg.text
 
     @mock.patch('banzai.cache.replication.create_engine')
     def test_uses_autocommit(self, mock_create_engine):
         engine, conn = _mock_engine()
         mock_create_engine.return_value = engine
         replication.setup_subscription('postgresql+psycopg://local/db', 'host=aws', site_id='ogg')
-        conn.execution_options.assert_called_once_with(isolation_level="AUTOCOMMIT")
+        conn.execution_options.assert_called_with(isolation_level="AUTOCOMMIT")
+
+    @mock.patch('banzai.cache.replication.create_engine')
+    def test_reuses_existing_slot(self, mock_create_engine):
+        engine, conn = _mock_engine()
+        mock_create_engine.return_value = engine
+        orig = pg_errors.ProtocolViolation('replication slot "banzai_lsc_slot" already exists')
+        slot_exists_error = OperationalError('', {}, orig)
+        conn.execute.side_effect = [slot_exists_error, None]
+        replication.setup_subscription('postgresql+psycopg://local/db', 'host=aws dbname=cal', site_id='lsc')
+        assert conn.execute.call_count == 2
+        retry_sql = conn.execute.call_args_list[1][0][0]
+        assert 'create_slot = false' in retry_sql.text
+
+    @mock.patch('banzai.cache.replication.create_engine')
+    def test_non_slot_operational_error_propagates(self, mock_create_engine):
+        engine, conn = _mock_engine()
+        mock_create_engine.return_value = engine
+        orig = Exception('connection refused')
+        conn.execute.side_effect = OperationalError('', {}, orig)
+        with pytest.raises(OperationalError):
+            replication.setup_subscription('postgresql+psycopg://local/db', 'host=aws', site_id='lsc')
