c9588fe16d571c8f175d9be4c3d751ff6b764688
Tim Beccue <timothybeccue@gmail.com>
2026-03-26 13:16:58 -0400

    Suppress opentsdb metrics connection warning by default

--- banzai/__init__.py
+++ banzai/__init__.py
@@ -3,8 +3,12 @@
 # Packages may add whatever they like to this file, but
 # should keep this content at the top.
 # ----------------------------------------------------------------------------
+import os
 import banzai.logs  # noqa: F401
 # ----------------------------------------------------------------------------
+
+# Remove useless warning: max retries exceeded on host scheduler-dev.lco.gtn
+os.environ.setdefault("OPENTSDB_PYTHON_METRICS_TEST_MODE", "1")
 
 try:
     import importlib.metadata as metadata
